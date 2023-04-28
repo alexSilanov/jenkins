1be0f36ea870e5ada00f5c8ee95e35345700c354
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2023-04-28 14:22:13 -0400

    add wait for jenkins (#3)
    * add wait for jenkins

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -4,6 +4,6 @@ describe('Jenkins Dashboard', () => {
     it('verify dropdown menu Dashboard', function() {
       cy.get('.jenkins-breadcrumbs__list-item > .model-link').should('have.text', 'Dashboard');
       cy.get('li.jenkins-breadcrumbs__list-item button.jenkins-menu-dropdown-chevron').click({force: true});
-      cy.get('#breadcrumb-menu>div.bd>ul.first-of-type>li>a>span').should('be.visible').and('have.length', 8);
+      cy.get('#breadcrumb-menu>div.bd>ul.first-of-type>li>a>span').should('be.visible').and('have.length', 5);
     });
 });
