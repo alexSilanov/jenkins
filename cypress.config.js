baf1312a898c84c3bafb07160ec9ca773966d722
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2023-04-27 23:15:27 -0400

    add test (#2)
    * add test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  video: false,
+    reporter: 'junit',
+    reporterOptions: {
+        mochaFile: 'reports/test-results-[hash].xml',
+    },
 });
