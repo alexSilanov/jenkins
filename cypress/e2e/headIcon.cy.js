d118e58e1eb0c6f225299ac84edf66f1c021bc3a
ellayezina <ellayjuly@gmail.com>
2023-05-10 19:35:00 -0400

    refactor

--- cypress/e2e/headIcon.cy.js
+++ cypress/e2e/headIcon.cy.js
@@ -1,8 +1,6 @@
 /// <reference types="cypress" />
-describe('Jenkins icon', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:8080/')
-    })
+
+describe('Head Icon', () => {
 
     it('Verify Jenkins icon redirection to the homepage', () => {
         cy.get('#jenkins-head-icon').click()
