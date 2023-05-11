6ce27f3584c506d30a946e0e10f90b8b2f29e2e2
Olesia Matveeva <118187575+matvlesia@users.noreply.github.com>
2023-05-11 10:54:50 +0100

    AT_5.05_001 | Create a new Multibranch Pipeline (#22)

--- cypress/e2e/newItemTest.cy.js
+++ cypress/e2e/newItemTest.cy.js
@@ -15,4 +15,13 @@ describe("newItemTest", () => {
         cy.get("[href$='Engineer/']").should("have.text", "Engineer");
     });
 
+    it('Create a new Multibranch Pipeline', () => {
+        cy.get('a[href="/view/all/newJob"]').click()
+        cy.get('#name').type('Multibranch Pipeline')
+        cy.get('.org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject').click()
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('#breadcrumbBar li:first-child').click()
+        cy.get('.jenkins-table__link.model-link.inside').should('have.text', 'Multibranch Pipeline')
+    });
 });
