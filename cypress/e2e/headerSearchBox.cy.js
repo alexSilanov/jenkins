86fe6ead759ee017f02dd68dc143f3c8c0d335d5
Yulia Lopatko <99161649+engineeryulia@users.noreply.github.com>
2023-05-11 14:25:23 -0600

    add AT_01.02_001 (#51)

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -5,4 +5,10 @@ describe('Header Search Box', () => {
     it('AT_01.02_003 | Verify a placeholder text “Search (CTRL+K)" in input field Search box', function () {
     cy.get('#search-box').should('have.attr', 'placeholder', 'Search (CTRL+K)')
     });
+
+    it('AT_01.02_001 | Verify that user navigate to Search Box documentation page', function () {
+        cy.get('.main-search__icon-trailing').invoke('removeAttr', 'target').click()
+        cy.url().should('include', '/doc/book/using/searchbox/')
+        cy.get('h1#search-box').should('contain.text', '\nSearch Box\n')
+    });
 })
