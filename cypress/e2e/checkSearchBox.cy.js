b6fa175ca3ca67fdf70cc4eaf6402aae4800fba5
SiarheiMSV <melnichak35@gmail.com>
2023-05-11 13:11:02 -0400

    changed2

--- cypress/e2e/checkSearchBox.cy.js
+++ cypress/e2e/checkSearchBox.cy.js
@@ -6,8 +6,7 @@ describe('Check seach box', () => {
     it('Check placeholder text "Search (CTRL+K)"', () => {
         cy.visit("http://localhost:8080/")
         cy.get('input#search-box')
-        .should('have.attr', 'placeholder', 'Search (⌘+K)')
+        .should('have.attr', 'placeholder', 'Search (CTRL+K)')
         .should('be.visible')
-        
     });
 });
--- cypress/e2e/verifyLogoutButton.cy.js
+++ cypress/e2e/verifyLogoutButton.cy.js
@@ -5,6 +5,5 @@ describe('Header > Log out button', () => {
     it('AT_01.08_002 | Verify log out button', () => {
         cy.get(' a[href="/logout"]').click()
         cy.get('h1').should('have.text', 'Welcome to Jenkins!')
-        
     });
 });
