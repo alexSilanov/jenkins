bc8cd1af012916f9ad211f311a489326bad3eede
Iryna <iryna.kolhanova@gmail.com>
2023-05-11 01:05:41 -0400

    refaxtor: TC_15.03_001 | <Folder> Edit description

--- cypress/e2e/folderAddDescription.cy.js
+++ cypress/e2e/folderAddDescription.cy.js
@@ -2,20 +2,17 @@
 
 describe('Folder', () => {
   it('verify that user can add description to Folder', function () {
-    // Precondition
+    
     cy.get('a[href="newJob"]').click();
     cy.get('input#name').type('TestProject');
     cy.get('li[tabindex="0"] span').contains('Folder').click();
     cy.get('#ok-button').click();
-    cy.url('/job/TestProject/configure');
     cy.get('button[name=Submit]').click();
-    cy.url('/job/TestProject');
-    cy.get('#main-panel > h1').should(($h1) => {
+    cy.get('#main-panel > h1').then(($h1) => {
       const text = $h1.text().trim();
       expect(text).to.equal('TestProject');
     });
-
-    // steps
+    
     cy.get('a#description-link').click();
     cy.get('textarea.jenkins-input').type('My description');
     cy.get('button[name=Submit]').click();
