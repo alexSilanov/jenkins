b90cf5424853ccc4ac9e995267178a8bff5b68f8
Alex Kushnir <classic.ua@gmail.com>
2023-05-10 22:09:37 -0700

    AT_15.02_001 | Add description in Folder (#25)

--- cypress/e2e/FolderAddDescription.cy.js
+++ cypress/e2e/FolderAddDescription.cy.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress"/>
 
-describe('Folder add description', () => {
-    it('TC 15.02_003 Add description', () => {
+describe('Folder > Add User description with "Add description" button', () => {
+  let textDescription = "User description";
+
+  afterEach(() => {
+    cy.get("#description-link").click();
+    cy.get(".jenkins-input").type("{selectall}").type("{backspace}");
+    cy.get(".jenkins-button").click();
+  });
+  
+      it('TC 15.02_003 Add description', () => {
         cy.get('.task-link').eq(1).click();
         cy.get('.jenkins-table__link').click();
         cy.get('#description-link').click();
@@ -9,4 +17,18 @@ describe('Folder add description', () => {
         cy.get('.jenkins-button').click();
         cy.get('#description > :nth-child(1)').should('contain', 'Hello world!');
     })
-})
+
+  it("AT 15.02.001 | Add User description", function () {
+    cy.get("a.task-link").eq(1).click();
+    cy.get(".jenkins-table__link").first().click();
+    cy.get("#description-link").click();
+    cy.get(".jenkins-input").type("{selectall}").type(textDescription);
+    cy.get(".jenkins-button").click();
+    cy.get("#description div:nth-of-type(1)").should(
+      "have.text",
+      textDescription
+    );
+  });
+});
+
+
