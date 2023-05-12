f61f492c912564b514b55423b031c1e5a2d7280e
Andrei Gaponenko <42135041+puh50@users.noreply.github.com>
2023-05-12 17:07:38 +0300

    AT_12.06_008 Tooltip with text "Disabled" appears (#91)

--- cypress/e2e/freestyleProjectDisable.cy.js
+++ cypress/e2e/freestyleProjectDisable.cy.js
@@ -245,4 +245,40 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 			.should('be.enabled')
 	});
 
+	it('AT_12.06_008 | Freestyle project. Disable project. Tooltip with text "Disabled" appears for the status icon of disabled project', () => {
+		// create Freestyle project
+		cy.intercept('/view/all/newJob').as('newJobsList')
+		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
+		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
+		cy.intercept(`/`).as('dashboard')
+		cy.get('a.task-link[href="/view/all/newJob"]').click()
+		cy.wait('@newJobsList')
+		cy.get('input#name').type(projects.freestyle.name)
+		cy.get('.hudson_model_FreeStyleProject').click()
+		cy.get('#ok-button').click()
+		cy.wait('@newProjectConfigure')
+		cy.get('[name="Submit"]').click()
+		cy.wait('@newProjectProfile')
+		cy.get('.page-headline').should('include.text', projects.freestyle.name)
+		cy.contains('a[href="/"]', 'Dashboard').click()
+		cy.wait('@dashboard')
+		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
+		// steps
+		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
+		cy.wait('@newProjectProfile')
+		cy.get('form#disable-project')
+			.contains('button[name="Submit"]', 'Disable Project')
+			.click()
+		cy.contains('a[href="/"]', 'Dashboard').click()
+		cy.wait('@dashboard')
+		cy.get('table#projectstatus')
+			.contains('tr', projects.freestyle.name)
+			.find('svg.icon-disabled')
+			.realHover()
+		//expected result
+		cy.get(`div[data-tippy-root]`)
+			.should('be.visible')
+			.and('have.text', 'Disabled')
+	});
+
 });
