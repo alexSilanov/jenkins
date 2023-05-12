0d2e413afdda60dab1464f408f59d87c0a0bdf60
adanilevskyy <andriy.danilevskyy@gmail.com>
2023-05-12 17:11:12 +0300

    AT_01.01_003 | Verify Jenkins icon and name-icon are visible (#38)
    * Head icon visibility test
    
    * updated test
    
    * updated head icon locator

--- cypress/e2e/headIcon.cy.js
+++ cypress/e2e/headIcon.cy.js
@@ -2,6 +2,11 @@
 
 describe('Head Icon', () => {
 
+    it('Verify Jenkins icon and name-icon are visible in header', () => {
+        cy.get('#jenkins-head-icon').should("be.visible")
+        cy.get('#jenkins-name-icon').should("be.visible")
+    })
+
     it('Verify Jenkins icon redirection to the homepage', () => {
         cy.get('#jenkins-head-icon').click()
         cy.url().should('include', 'localhost')
