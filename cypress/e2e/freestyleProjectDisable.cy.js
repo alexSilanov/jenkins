877ae1060d22721afa16cd155d7f5fc9f46fe0f7
Andrei Gaponenko <42135041+puh50@users.noreply.github.com>
2023-05-12 15:09:39 +0300

    AT_12.06_006 "Enable" button has blue color (#87)

--- cypress/e2e/freestyleProjectDisable.cy.js
+++ cypress/e2e/freestyleProjectDisable.cy.js
@@ -177,4 +177,41 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 			.should('be.visible')
 			.and('have.text', 'Enable')
 	});
+
+	it('AT_12.06_006 | Freestyle project. Disable project. "Enable" button has blue color', () => {
+		// create Freestyle project
+		cy.intercept('/view/all/newJob').as('newJobsList')
+		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
+		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
+		cy.intercept(`/`).as('dashboard')
+		cy.get('a.task-link[href="/view/all/newJob"]').click()
+		cy.wait('@newJobsList')
+		cy.get('input#name').type(projects.freestyle.name)
+		cy.get('.hudson_model_FreeStyleProject').click()
+		cy.get('#ok-button').click()
+		cy.wait('@newProjectConfigure')
+		cy.get('[name="Submit"]').click()
+		cy.wait('@newProjectProfile')
+		cy.get('.page-headline').should('include.text', projects.freestyle.name)
+		cy.contains('a[href="/"]', 'Dashboard').click()
+		cy.wait('@dashboard')
+		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
+		// steps
+		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
+		cy.wait('@newProjectProfile')
+		cy.get('form#disable-project')
+			.contains('button[name="Submit"]', 'Disable Project')
+			.click()
+		//expected result
+		cy.get('form#enable-project')
+			.find('button[name="Submit"]')
+			.should('have.text', 'Enable')
+			.and('be.visible')
+			.and(($el) => {
+				const win = $el[0].ownerDocument.defaultView // get Window reference from element
+				const before = win.getComputedStyle($el[0], 'before') // use getComputedStyle to read the pseudo selector
+				const contentValue = before.getPropertyValue('background-color') // read the value of the CSS property
+				expect(contentValue).to.eq('rgb(0, 98, 209)') // blue color
+			})
+	});
 })
