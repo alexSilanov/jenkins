998f7f76c0bdd8ddbe295ac6f76d5e277aa1c9e7
Ekaterina Blinova <132958390+EkBlinova@users.noreply.github.com>
2023-05-11 22:23:48 -0700

    AT_05.02_002 | Create a new Pipeline (#60)
    * AT_05.02_002 | Create a new Pipeline

--- cypress/e2e/newPipelineCreate.cy.js
+++ cypress/e2e/newPipelineCreate.cy.js
@@ -8,4 +8,14 @@ describe('Create a new Pipeline', () => {
         cy.get('#breadcrumbBar li:first-child').click()
         cy.get('table#projectstatus').should('contain', newPipeLineName)
     })
+
+    it('Create a new Pipeline', () => {
+        cy.contains('span[class="task-link-text"]', 'New Item').click({force:true})
+        cy.get('.org_jenkinsci_plugins_workflow_job_WorkflowJob').click()
+        cy.get('.jenkins-input').type('New project')
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('li.jenkins-breadcrumbs__list-item').click()
+        cy.get('a[href*="job/New%20project"]').should('be.visible')
+    })
 })
