15a13ca3bc13dcafcf0176c14589253c7860c06a
LinaAbu1306 <Lina.qa.abu@gmail.com>
2023-05-11 23:42:54 -0400

    AT_3.01.003 | Footer | Verify REST API Redirect (#71)

--- cypress/e2e/verificationOfLinkRestAPI.cy.js
+++ cypress/e2e/verificationOfLinkRestAPI.cy.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress"/>
-describe('<Footer> Verify Link REST API',()=>{
-    it('Rest API link redirecting to the correct page',()=>{
-        cy.get('a[href="api/"]').click()
-        cy.url().should('includes','/api/')
-    })
-})
+
+import projects from "../fixtures/projects.json";
+
+const PORT = Cypress.env("local.port");
+
+describe("Footer | Verify Link REST API", () => {
+    
+  it("Rest API link redirecting to the correct page", () => {
+    cy.get('a[href="api/"]').click();
+    cy.url().should("includes", "/api/");
+  });
+
+  it("AT_3.01.003 | Footer | Verify REST API Redirect", () => {
+    cy.get('a[href="api/"]').click();
+    cy.url().should("include", `http://localhost:${PORT}/api/`);
+    cy.get("#main-panel h1").should("have.text", projects.api.text);
+  });
+});
