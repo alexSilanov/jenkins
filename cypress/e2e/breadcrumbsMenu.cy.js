436e6b2a59f42d68a402b88e5d96e606272c2aba
IrynaKolyada <iryna.kolyada.us@gmail.com>
2023-05-12 17:44:37 -0600

    test added (#112)

--- cypress/e2e/breadcrumbsMenu.cy.js
+++ cypress/e2e/breadcrumbsMenu.cy.js
@@ -4,6 +4,8 @@ describe('Breadcrumbs',()=>{
     
   const pagesName =['New Item', 'People', 'Build History', 'Manage Jenkins', 'My Views']
   const endPointUrl = ['/all/newJob', '/asynchPeople/', '/builds' ,'/manage/', '/my-views/view/all/']
+  const text = "Jenkins project"
+  const text2 = "Jenkins project in progress"
 
     it('AT_04.02_001 |Dashboard is displayed on every page and user is able to go back to Home page', ()=>{
    
@@ -29,4 +31,14 @@ describe('Breadcrumbs',()=>{
            cy.go('back')
         })
      })
+
+     it('AT_02.06_002 |Homepage (Dashboard) User is able to add and edit the text in the panel description', () => {
+      cy.get('#description-link').should('be.visible').click()
+      cy.get('textarea[name="description"]').clear().type(text).should('have.value', text)
+      cy.get('button[name="Submit"]').click()
+      cy.get('a[href="editDescription"]').click()
+      cy.get('textarea[name="description"]').clear().type(text2).should('have.value', text2)
+      cy.get('button[name="Submit"]').click()
+   })
+
 })
