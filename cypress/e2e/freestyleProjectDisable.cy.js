d3c42887cf68518434c22794c49e23215619a538
Andrei Gaponenko <42135041+puh50@users.noreply.github.com>
2023-05-12 13:49:13 +0300

    AT_12.06_0004 Disable project icon is grayed out (#84)
    * AT_12.06_0004 Disable project icon is grayed out

--- cypress/e2e/freestyleProjectDisable.cy.js
+++ cypress/e2e/freestyleProjectDisable.cy.js
@@ -112,4 +112,38 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 			.and('have.attr', 'title', 'Disabled')
 	});
 
+	it('AT_12.06_0004 | Freestyle project>Disable project. "Disabled" status icon has grey color', () => {
+		cy.intercept('/view/all/newJob').as('newJobsList')
+		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
+		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
+		cy.intercept(`/`).as('dashboard')
+		cy.get('a.task-link[href="/view/all/newJob"]').click()
+		cy.wait('@newJobsList')
+		cy.get('input#name').type(projects.freestyle.name)
+		cy.get('.hudson_model_FreeStyleProject').click()
+		cy.get('#ok-button').click()
+		cy.wait('@newProjectConfigure')
+		cy.get('[name="Submit"]').click()
+		cy.wait('@newProjectProfile')
+		cy.get('.page-headline').should('include.text', projects.freestyle.name)
+		cy.contains('a[href="/"]', 'Dashboard').click()
+		cy.wait('@dashboard')
+		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
+		// steps
+		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
+		cy.wait('@newProjectProfile')
+		cy.get('form#disable-project')
+			.contains('button[name="Submit"]', 'Disable Project')
+			.click()
+		cy.contains('a[href="/"]', 'Dashboard').click()
+		cy.wait('@dashboard')
+		//expected result
+		cy.get('table#projectstatus')
+			.contains('tr', projects.freestyle.name)
+			.find('svg.icon-disabled')
+			.should('be.visible')
+			.and('have.css', 'fill', 'rgb(20, 20, 31)')
+			.and('have.css', 'color', 'rgb(20, 20, 31)')
+			.and('have.css', 'opacity', '0.5')
+	});
 })
