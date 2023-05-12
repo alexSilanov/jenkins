8822c097620a874f6b0d9f09b2168a4e31c54ab1
Andrei Gaponenko <42135041+puh50@users.noreply.github.com>
2023-05-12 14:34:04 +0300

    AT_12.06_005 "Enable" btn appears (#85)

--- cypress/e2e/freestyleProjectDisable.cy.js
+++ cypress/e2e/freestyleProjectDisable.cy.js
@@ -146,4 +146,35 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 			.and('have.css', 'color', 'rgb(20, 20, 31)')
 			.and('have.css', 'opacity', '0.5')
 	});
+
+	it('AT_12.06_005 | Freestyle project. Disable project. "Enable" button appears after the project is disabled', () => {
+		// create Freestyle project
+		cy.intercept('/view/all/newJob').as('newJobsList')
+		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
+		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
+		cy.intercept(`/`).as('dashboard')
+		cy.get('a.task-link[href="/view/all/newJob"]').click()
+		cy.wait('@newJobsList')
+		cy.get('input#name').type(projects.freestyle.name)
+		cy.get('.hudson_model_FreeStyleProject').click()
+		cy.get('#ok-button').click()
+		cy.wait('@newProjectConfigure')
+		cy.get('[name="Submit"]').click()
+		cy.wait('@newProjectProfile')
+		cy.get('.page-headline').should('include.text', projects.freestyle.name)
+		cy.contains('a[href="/"]', 'Dashboard').click()
+		cy.wait('@dashboard')
+		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
+		// steps
+		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
+		cy.wait('@newProjectProfile')
+		cy.get('form#disable-project')
+			.contains('button[name="Submit"]', 'Disable Project')
+			.click()
+		//expected result
+		cy.get('form#enable-project')
+			.find('button[name="Submit"]')
+			.should('be.visible')
+			.and('have.text', 'Enable')
+	});
 })
