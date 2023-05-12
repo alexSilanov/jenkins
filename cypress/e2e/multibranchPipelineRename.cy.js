8ade4a4ea1bc6ab7bd66dd5c03e9f4a414906ab2
Gayana-G <72823317+Gayana-G@users.noreply.github.com>
2023-05-12 14:36:23 -0400

    AT_16.02_001 | <Multibranch Pipeline> Rename Multibranch Pipeliner using dropdown menu (positive) (#99)
    Co-authored-by: g.futorka@gmail.com <Galustyn3009>

---
+++ cypress/e2e/multibranchPipelineRename.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+import pipelineName from "../fixtures/pipelineName.json";
+
+describe('Rename existing Multibranch Pipeline', () =>{
+    beforeEach(() => {
+        cy.get('a[href$="/newJob"]').click();
+        cy.get('.jenkins-input').as('nameInputField');
+        cy.get('@nameInputField').type(pipelineName.namePipeline);
+        cy.get('[class="label"]').contains('Multibranch Pipeline').click();
+        cy.get('button[type=submit]').should('be.enabled').click();
+        cy.url().should('include', `/${pipelineName.namePipeline}/configure`);
+        cy.get('button[name=Submit]').click();
+        cy.get('[class="model-link"]').contains('Dashboard').click();
+    })
+
+    it('Rename Multibranch Pipeline using dropdown menu_positive', ()=> {
+        cy.get('a[href^="job/"').realHover();
+        cy.get('td > a [class$="dropdown-chevron"]').click();
+        cy.get('li > a > span').contains('Rename').click();
+        cy.get('@nameInputField').clear();
+        cy.get('@nameInputField').type(pipelineName.newNamePipeline);
+        cy.get('button[name=Submit]').click();
+        cy.url().should('include', `/job/${pipelineName.newNamePipeline}`);
+        cy.get('h1').contains(`${pipelineName.newNamePipeline}`);
+    })
+})
