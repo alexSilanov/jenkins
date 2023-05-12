0e5fd71652f68f12627792ac4be056d7ecc90023
Alex Kushnir <classic.ua@gmail.com>
2023-05-11 20:09:02 -0700

    peope tab (#64)

--- cypress/e2e/verifyLogOutButton.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress"/>
-
-describe('<Header> Log out button',()=>{
-    it('Verify presence of log out button',()=>{
-        cy.get('div[class="login page-header__hyperlinks"] :nth-child(4)').should('have.text','log out')
-    })
-})
