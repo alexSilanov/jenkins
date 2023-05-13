992c54eee936ea28eea467a93ab605baad515c61
YuliaMatusevich <yuliya.matusevich.qa@gmail.com>
2023-05-12 22:43:28 -0500

    Test 'My views Edit Description text is saved'

--- cypress/e2e/myViewsDescription.cy.js
+++ cypress/e2e/myViewsDescription.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress"/>
 
 describe('myViewsEditDescriptionTest', () => {
-    let description = 'text'
+    const description = 'text'
+    const newDescription = 'newText'
 
     it ('My views Edit Description', () => {
         cy.contains('My Views').click()
@@ -11,5 +12,18 @@ describe('myViewsEditDescriptionTest', () => {
         cy.get('#description-link').should("have.attr", "href")
         cy.get('#description-link').invoke('attr', 'href').should('eq', 'editDescription')
         cy.get('.jenkins-buttons-row').should('contain', 'Edit description')
+    })
+
+    it ('My views Edit Description text is saved', () => {
+        cy.contains('My Views').click()
+        cy.get('#description-link').click()
+        cy.get('.jenkins-input   ').clear().type(description)
+        cy.get('.jenkins-button').click()
+        cy.get('#description div:nth-child(1)').should('have.text', description)
+        cy.get('a[href="editDescription"]').click()
+        cy.get('.jenkins-input   ').clear().type(newDescription)
+        cy.get('.jenkins-button').click()
+        cy.get('#description div:nth-child(1)').should('have.text', newDescription)
+        cy.get('#description div:nth-child(1)').should('not.have.text', description)
     })
 })
