ad74f171801e44d76d40230e35d12fa5ad6fbf7a
Olha Dymaretska <olhadymaretska@gmail.com>
2023-05-12 23:51:12 -0400

    Verify Search box is accessible from every page

---
+++ cypress/e2e/pageHeaderSearchBox.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress"/>
+
+describe('Jenkins Search Box', () => {
+    it('verify Search Box is visible on the home page', function () {
+        cy.get('#searchform').should('be.visible').click().type('project')
+        cy.get('.main-search__icon-leading').should('have.css','width','40px','height','40px' )
+
+    });
+
+    it('verify Search Box is visible on the New Item page', function () {
+        cy.get('.task  a[href="/view/all/newJob"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+        cy.get('.main-search__icon-leading').should('have.css','width','40px','height','40px' )
+    });
+
+    it('verify Search Box is visible on the People page', function () {
+        cy.get('.task  a[href="/asynchPeople/"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+        cy.get('.main-search__icon-leading').should('have.css','width','40px','height','40px' )
+
+    });
+
+    it('verify Search Box is visible on the Build History page', function() {
+        cy.get('.task a[href="/view/all/builds"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+    })
+    it('verify Search Box is visible on the Manage Jenkins page', function() {
+        cy.get('.task a[href="/manage"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+    })
+    it('verify Search Box is visible on the My Views page', function() {
+        cy.get('.task a[href="/me/my-views"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+    })
+   
+    it('verify Search Box is visible on the Node Monitoring page', function() {
+        cy.get('.task a[href="configure"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+    })
+
+    it('verify Search Box is visible on the API page', function() {
+        cy.get('a[href="api/""]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+    })
+
+    it('verify Search Box is visible on the User page', function() {
+        cy.get('a[class="model-link"] span[class="hidden-xs hidden-sm"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project')
+    })
+});
