cdefe15e0269860ed013bcc8bcd6570e445f1bc0
Andrei Gaponenko <42135041+puh50@users.noreply.github.com>
2023-05-13 14:43:22 +0300

    [RF] US_12.06 Disable project tests refactoring (#123)

--- cypress/e2e/freestyleProjectDisable.cy.js
+++ cypress/e2e/freestyleProjectDisable.cy.js
@@ -4,8 +4,7 @@ import projects from '../fixtures/projects.json'
 
 describe('US_12.06 | Freestyle project Disable project', () => {
 
-	it('AT_12.06_001 | Freestyle project "Disable project" option exists', () => {
-		// create Freestyle project
+	beforeEach(() => {
 		cy.intercept('/view/all/newJob').as('newJobsList')
 		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
 		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
@@ -22,81 +21,32 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 		cy.contains('a[href="/"]', 'Dashboard').click()
 		cy.wait('@dashboard')
 		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
+	})
+
+	it('AT_12.06_001 | Freestyle project "Disable project" option exists', () => {
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
-		cy.get('form#disable-project').find('button[name="Submit"]')
+		//expected result
+		cy.get('form#disable-project')
+			.find('button[name="Submit"]')
 			.should('have.text', 'Disable Project')
 			.and('be.visible')
 			.and('be.enabled')
 	});
 
-	it('AT_12.06_002 | Freestyle project. Project is disabled after clicking "Disable project" button (project profile asserts)', () => {
-		// create Freestyle project
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
+	it('AT_12.06_002 | Freestyle project. "This project is currently disabled" notification appears after clicking "Disable project" button in the project profile', () => {
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
 			.contains('button[name="Submit"]', 'Disable Project')
 			.click()
 		//expected result
-		cy.get('form#enable-project')
+		cy.get('.warning')
 			.should('be.visible')
 			.and('include.text', projects.disabledProjectNotify)
-		cy.get('form#enable-project')
-			.find('button[name="Submit"]')
-			.should('have.text', 'Enable')
-		cy.get('form#enable-project')
-			.find('button[name="Submit"]')
-			.should(($el) => {
-				// get Window reference from element
-				const win = $el[0].ownerDocument.defaultView
-				// use getComputedStyle to read the pseudo selector
-				const before = win.getComputedStyle($el[0], 'before')
-				// read the value of the `content` CSS property
-				const contentValue = before.getPropertyValue('background-color')
-				// the returned value will have double quotes around it, but this is correct
-				expect(contentValue).to.eq('rgb(0, 98, 209)') // blue color
-			})
-			.and('be.visible')
-			.and('be.enabled')
 	});
 
 	it('AT_12.06_003 | Freestyle project. Project Status is changed to "Disabled" on Dashboard after clicking "Disable project" button', () => {
-		// create Freestyle project
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
@@ -112,24 +62,7 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 			.and('have.attr', 'title', 'Disabled')
 	});
 
-	it('AT_12.06_0004 | Freestyle project>Disable project. "Disabled" status icon has grey color', () => {
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
+	it('AT_12.06_004 | Freestyle project. Disable project. "Disabled" status icon has grey color', () => {
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
@@ -148,24 +81,6 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 	});
 
 	it('AT_12.06_005 | Freestyle project. Disable project. "Enable" button appears after the project is disabled', () => {
-		// create Freestyle project
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
@@ -179,24 +94,6 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 	});
 
 	it('AT_12.06_006 | Freestyle project. Disable project. "Enable" button has blue color', () => {
-		// create Freestyle project
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
@@ -216,24 +113,6 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 	});
 
 	it('AT_12.06_007 | Freestyle project. Disable project. "Enable" button is clickable (not disabled)', () => {
-		// create Freestyle project
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
@@ -246,24 +125,6 @@ describe('US_12.06 | Freestyle project Disable project', () => {
 	});
 
 	it('AT_12.06_008 | Freestyle project. Disable project. Tooltip with text "Disabled" appears for the status icon of disabled project', () => {
-		// create Freestyle project
-		cy.intercept('/view/all/newJob').as('newJobsList')
-		cy.intercept(`/job/${projects.freestyle.name}/configure`).as('newProjectConfigure')
-		cy.intercept(`/job/${projects.freestyle.name}/`).as('newProjectProfile')
-		cy.intercept(`/`).as('dashboard')
-		cy.get('a.task-link[href="/view/all/newJob"]').click()
-		cy.wait('@newJobsList')
-		cy.get('input#name').type(projects.freestyle.name)
-		cy.get('.hudson_model_FreeStyleProject').click()
-		cy.get('#ok-button').click()
-		cy.wait('@newProjectConfigure')
-		cy.get('[name="Submit"]').click()
-		cy.wait('@newProjectProfile')
-		cy.get('.page-headline').should('include.text', projects.freestyle.name)
-		cy.contains('a[href="/"]', 'Dashboard').click()
-		cy.wait('@dashboard')
-		cy.get('table#projectstatus').should('include.text', projects.freestyle.name)
-		// steps
 		cy.get(`a[href="job/${projects.freestyle.name}/"]`).click()
 		cy.wait('@newProjectProfile')
 		cy.get('form#disable-project')
