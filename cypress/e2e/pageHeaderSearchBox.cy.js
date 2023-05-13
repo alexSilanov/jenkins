a645aa1cab620b8a2981eefbc042feb96b8f3bec
Olha Dymaretska <olhadymaretska@gmail.com>
2023-05-13 12:10:32 -0400

    AT_01.02_005 | Verify Search box

--- cypress/e2e/pageHeaderSearchBox.cy.js
+++ cypress/e2e/pageHeaderSearchBox.cy.js
@@ -3,20 +3,16 @@
 describe('Jenkins Search Box', () => {
     it('verify Search Box is visible on the home page', function () {
         cy.get('#searchform').should('be.visible').click().type('project')
-        cy.get('.main-search__icon-leading').should('have.css','width','40px','height','40px' )
-
     });
 
     it('verify Search Box is visible on the New Item page', function () {
         cy.get('.task  a[href="/view/all/newJob"]').click()
-        cy.get('#searchform').should('be.visible').click().type('project')
-        cy.get('.main-search__icon-leading').should('have.css','width','40px','height','40px' )
+        cy.get('#searchform').should('be.visible').click().type('project') 
     });
 
     it('verify Search Box is visible on the People page', function () {
         cy.get('.task  a[href="/asynchPeople/"]').click()
         cy.get('#searchform').should('be.visible').click().type('project')
-        cy.get('.main-search__icon-leading').should('have.css','width','40px','height','40px' )
 
     });
 
