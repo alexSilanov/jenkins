2fd38f19d6574d08ae57a0f726be454eb40970b8
Anna Derkach18 <83091566+Ann-des@users.noreply.github.com>
2023-05-14 22:32:41 +0100

    AT_01.03_004| Header| User icon| Select menuOption (#158)
    Co-authored-by: Anna.Derkach <anna.derkach@hrforecast.de>

--- cypress/e2e/headerUserIcon.cy.js
+++ cypress/e2e/headerUserIcon.cy.js
@@ -14,4 +14,11 @@ describe('Header User Icon', () => {
     it('AT_01.03_005 | Header | User icon | Verification of the visibility of the user icon', function () {
         cy.get('.login.page-header__hyperlinks .model-link').should('be.visible');
     });
+
+    it('AT_01.03_004| Header | User icon | Ability to choose one of the menu-list option by clicking on it', function () {
+        cy.get('a[href= "/user/admin"] button').click({force:true})
+        cy.get('ul.first-of-type').should('be.visible')
+        cy.get('ul.first-of-type span').contains('Builds').click()
+        cy.url().should('contain', '/user/admin/builds')
+    })
 })
