d0669e7c7340bffd15226ef10d94b7602a6818ce
Andrey Bozhko <556070@gmail.com>
2023-05-14 16:54:30 +0300

    AT_01.04.02 | <Header>User Builds link > Verify redirected to the “Builds for User" page

--- cypress/e2e/headerUserBuildsLink.cy.js
+++ cypress/e2e/headerUserBuildsLink.cy.js
@@ -5,5 +5,15 @@ describe('Header - User Builds Link', () => {
       cy.get('a[href="/user/admin"] button.jenkins-menu-dropdown-chevron').click({force:true})
       cy.get('li.yuimenuitem a[href="/user/admin/builds"]').should('be.visible').and('include.text', 'Builds').click()
     });
+    
+    it('Verify redirected to the “Builds for User" page', function () {
+      cy.get('a[href="/user/admin"] button.jenkins-menu-dropdown-chevron').click({force:true})
+      cy.get('li.yuimenuitem a[href="/user/admin/builds"]').click()
+      cy.url().should("contain", "/user/admin/builds")
 
+      cy.get('a.model-link span.hidden-xs.hidden-sm').then(($span) => {
+          const userName = $span.text()
+          cy.get('h1').should('have.text', 'Builds for ' + userName)
+        })
+  }); 
 });
