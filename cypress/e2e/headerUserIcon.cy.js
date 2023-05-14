b6777b8e22a10d4c1cbdaf094b5977364eacee70
viktoriashidlovskaya <121621494+viktoriashidlovskaya@users.noreply.github.com>
2023-05-14 13:05:18 +0300

    AT_01.03_003| Header | User icon | Dropdown menu (#151)

--- cypress/e2e/headerUserIcon.cy.js
+++ cypress/e2e/headerUserIcon.cy.js
@@ -5,4 +5,9 @@ describe('Header User Icon', () => {
     it('AT_01.03_001 | Verify “User icon” is visible on the right side of the header', function () {
     cy.get('.login .model-link').should('be.visible');
     });
+
+    it('AT_01.03_003| Header | User icon | Verification of the visibility of the dropdown menu', function () {
+        cy.get('a[href= "/user/admin"] button').click({force:true})
+        cy.get('ul.first-of-type').should('be.visible')
+    })
 })
