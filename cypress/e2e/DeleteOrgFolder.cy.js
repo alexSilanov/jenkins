2b788c4d24bf20af84c6f5d3c2523c5799c8ff29
Alexandr Sdobnov <126351854+SawkaQA@users.noreply.github.com>
2023-05-14 18:52:28 +0300

    AT_17.03_002 | Organization Folder (#90)
    * AT_5.06_002 | New Item Create Folder

---
+++ cypress/e2e/DeleteOrgFolder.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress"/>
+
+const folderName = 'Test';
+
+describe('Organization Folder > Delete Organization Folder', () => {
+
+    it('New Item > Create a new Organization Folder by [+New Item]', function () {
+
+    // Create Org Folder
+    cy.contains('New Item').click()
+
+    cy.url().should('eq', `http://localhost:${Cypress.env('local.port')}/view/all/newJob`)
+
+    cy.get("#name").type(folderName)
+
+    cy.contains("Organization Folder").click()
+
+    cy.get('#ok-button')
+    .should('be.visible')
+    .click()
+
+    cy.url()
+    .should('eq', `http://localhost:${Cypress.env('local.port')}/job/${folderName}/configure`)
+
+    cy.contains('Save').click()
+
+    cy.get('#main-panel > h1').should('contain', folderName)
+
+    cy.get(':nth-child(1) > .model-link').click()
+
+    cy.get('#job_Test .jenkins-table__link')
+    .should('have.text', folderName)
+    
+    // Delete Org Folder
+    cy.contains(folderName).click()
+    
+    cy.url()
+    .should('eq', `http://localhost:${Cypress.env('local.port')}/job/${folderName}/`)
+    
+    cy.contains("Delete Organization Folder").click()
+    
+    cy.get('[class$="color"]')
+    .should('be.visible')
+    .click()
+
+    })
+ })
