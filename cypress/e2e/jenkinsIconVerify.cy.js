90b345d5e2be0ee7d1fdfc83630b65e81596ae2b
Valentin Kushnarev <121609422+valentin-kushnarev@users.noreply.github.com>
2023-05-14 14:35:58 +0400

    AT_01.01_010 | Verify Jenkins (#124)
    * AT_01.01_010 | Verify Jenkins
    
    * Update jenkinsIconVerify.cy.js
    
    * AT_01.01_010 | Verify Jenkins | version_002

--- cypress/e2e/jenkinsIconVerify.cy.js
+++ cypress/e2e/jenkinsIconVerify.cy.js
@@ -11,4 +11,17 @@ describe('Header', () => {
         cy.get('#jenkins-head-icon').should('have.css', 'height', '40px')
         cy.get('#jenkins-head-icon').should('have.css', 'vertical-align', 'middle')
     });
-});
+
+    
+    it.only('Verify logo jenkins is visible on the header', function () {
+        cy.get('#jenkins-head-icon')
+          .should('be.visible')
+          .click()
+        
+        cy.url()
+          .should('contain', 'localhost')
+
+        cy.get('.empty-state-block h1')
+          .should('contain', 'Welcome to Jenkins!')
+    });
+})
