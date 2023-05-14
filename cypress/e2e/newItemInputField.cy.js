cf82f5ad899b33277c89ebbd0d62e479389ba5c4
Tatsiana-Nazarava <122973792+Tatsiana-Nazarava@users.noreply.github.com>
2023-05-14 12:53:32 +0300

    AT_05.07 _002| Verify New Item page contains input field (#128)

--- cypress/e2e/newItemInputField.cy.js
+++ cypress/e2e/newItemInputField.cy.js
@@ -1,10 +1,18 @@
 /// <reference types="cypress"/>
 
+const PORT = Cypress.env("local.port");
+
 describe('New Item', () => {
 
     it('Verify New Item has input field', () => {
         cy.get('a[href="/view/all/newJob"]').click()
         cy.url().should('include', '/view/all/newJob')
         cy.get('.jenkins-input').should('be.visible')
+    })
+    
+    it('AT_05.07 _002 | Verify New Item page contains input field', () => {
+        cy.get('a[href ="/view/all/newJob"]').click()
+        cy.url().should('include', `http://localhost:${PORT}/view/all/newJob`)
+        cy.get('#name').should('be.visible')
     })
 })
