eb85f310903a3683dc22e165f0fbf3f3b651b9b8
TatianaBM <130609785+TatianaBM@users.noreply.github.com>
2023-05-14 15:41:27 +0200

    AT_17.03_005| Delete Organization Folde (#145)
    * AT_17.03_005| Delete Organization Folde
    
    * correct_project_name

--- cypress/e2e/orgFolderDelete.cy.js
+++ cypress/e2e/orgFolderDelete.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress"/>
+import projects from '../fixtures/projects.json'
+import messages from '../fixtures/messages.json'
 
 describe('<Organization Folder> Delete Organization Folder', () => {
 
@@ -14,4 +16,20 @@ describe('<Organization Folder> Delete Organization Folder', () => {
         cy.get('div#breadcrumb-menu ul li a').contains('Delete Organization Folder').click();
         cy.get('button[name="Submit"]').click()
     });
+
+    it('Delete Organization Folder using dropdown menu', () => {
+        cy.get('#tasks .task').contains('New Item').click()
+        cy.get('input#name').type(projects.organizationFolder.name)
+        cy.get('#j-add-item-type-nested-projects li:last-child').click()
+        cy.get('#ok-button').click()
+        cy.get('#breadcrumbBar').contains('Dashboard').click()
+    
+        cy.get('tbody tr td .jenkins-table__link').contains(projects.organizationFolder.name).realHover()
+        cy.get('#projectstatus button.jenkins-menu-dropdown-chevron').click()
+        cy.get('.first-of-type li').contains('Delete Organization Folder').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('#breadcrumbBar').contains('Dashboard').click()
+        cy.get('#search-box').type(`${projects.organizationFolder.name}{enter}`)
+        cy.get('.error').should('have.text', messages.error)
+    })
 });
