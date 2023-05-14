ef70d6b7a3f2cfa1d6a716905a0d3ddeb6920cc1
a-silanov <65155747+a-silanov@users.noreply.github.com>
2023-05-14 11:37:06 +0200

    AT_01.10_002| Check Header Notification icon (#141)
    * AT_01.10_002| Header Notification icon

---
+++ cypress/e2e/checkNotificationIcon.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Header | notification icon', ()=> {
+    const buttons = ["Set up agent", "Set up cloud", "Dismiss"]
+    it('verify buttons and links', ()=> {
+        cy.get('#visible-sec-am-button')
+          .should('be.visible')
+          .click()
+        cy.get('.jenkins-button.jenkins-button--primary ')
+          .should('have.length', buttons.length)
+          .then(($els)=> {
+              return Cypress._.map($els, 'innerText')
+            })
+          .should('deep.equal', buttons)
+        cy.get('#visible-sec-am-list a[href="/manage"]').should('have.text', 'Manage Jenkins')
+    })
+})
