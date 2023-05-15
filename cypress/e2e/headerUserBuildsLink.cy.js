9031a3eb9344b8a4514493dbbfe00862ce9b55b4
AndreyBozhko <124488708+AndrSam1@users.noreply.github.com>
2023-05-15 17:58:33 +0300

    RF_AT_01.04.01 (#180)

--- cypress/e2e/headerUserBuildsLink.cy.js
+++ cypress/e2e/headerUserBuildsLink.cy.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress"/>
 
 describe('Header - User Builds Link', () => {
-    it('Verify menu item “Builds“ in “User” dropdown-menu displayed and clickable', function () {
-      cy.get('a[href="/user/admin"] button.jenkins-menu-dropdown-chevron').click({force:true})
-      cy.get('li.yuimenuitem a[href="/user/admin/builds"]').should('be.visible').and('include.text', 'Builds').click()
-    });
+  it('Verify menu item “Builds“ in “User” dropdown-menu displayed and clickable', () => {
+    cy.get('div.login.page-header__hyperlinks button.jenkins-menu-dropdown-chevron').click({force:true})
+    cy.get('li.yuimenuitem a span').contains('Builds').should('be.visible').and('include.text', 'Builds').click()
+  });
     
     it('Verify redirected to the “Builds for User" page', function () {
       cy.get('a[href="/user/admin"] button.jenkins-menu-dropdown-chevron').click({force:true})
