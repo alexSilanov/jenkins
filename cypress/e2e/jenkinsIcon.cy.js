4a43057c8ef73751d7d40a8e7f58625eba57f9d1
AllaIg <115178653+AllaIg@users.noreply.github.com>
2023-05-15 17:47:06 -0500

    AT_01.01.022 | Verify Jenkins Icon (#194)
    * AT_01.01.022 | Verify Jenkins Icon
    
    * removed port

--- cypress/e2e/jenkinsIcon.cy.js
+++ cypress/e2e/jenkinsIcon.cy.js
@@ -7,4 +7,12 @@ describe('Header - head icon', () => {
       cy.get('div h1').should('be.visible');
       cy.get('#jenkins-name-icon').should('be.visible');
     });
+
+    it('verify jenkins icon', function () {
+      cy.get('#jenkins-name-icon').should('be.visible');
+      cy.get('#jenkins-head-icon').should('be.visible');
+      cy.get('#jenkins-name-icon').click();
+      cy.url().should('contain' ,'localhost')
+    })
+
 });
