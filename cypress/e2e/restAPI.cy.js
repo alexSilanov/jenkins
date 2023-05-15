31edb0faead849e6f340b4f3129f2beee33e4ac9
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2023-05-15 16:10:13 +0200

    AT_03.01_005 | <Footer> Check the link REST API (#186)
    * Footer Check the link REST API
    
    * refactor

--- cypress/e2e/restAPI.cy.js
+++ cypress/e2e/restAPI.cy.js
@@ -5,4 +5,11 @@ describe('AT_3.01_004 | Verify the link "Rest API"', () =>{
         cy.get('a[href="api/"]').should('exist').should('have.text','REST API').click()
         cy.url().should('includes','/api/')
     })
+
+    it("AT_03.01_005 | <Footer> Check the link REST API", () => {
+    cy.get('[href="api/"]').click();
+    cy.url()
+      .should("be.eq", `http://localhost:${Cypress.env('local.port')}/api/`)
+    cy.get("div h1").should("have.text", "REST API");
+    });
 })
