e6eade1d960031ba6001bd606ee433fa694b4fe9
AndreyBozhko <124488708+AndrSam1@users.noreply.github.com>
2023-05-15 22:45:34 +0300

    [RF] AT_01.04.02 | <Header>User Builds link > Verify redirected to the “Builds for User" page (#189)

--- cypress/e2e/headerUserBuildsLink.cy.js
+++ cypress/e2e/headerUserBuildsLink.cy.js
@@ -6,14 +6,14 @@ describe('Header - User Builds Link', () => {
     cy.get('li.yuimenuitem a span').contains('Builds').should('be.visible').and('include.text', 'Builds').click()
   });
     
-    it('Verify redirected to the “Builds for User" page', function () {
-      cy.get('a[href="/user/admin"] button.jenkins-menu-dropdown-chevron').click({force:true})
-      cy.get('li.yuimenuitem a[href="/user/admin/builds"]').click()
-      cy.url().should("contain", "/user/admin/builds")
-
-      cy.get('a.model-link span.hidden-xs.hidden-sm').then(($span) => {
-          const userName = $span.text()
-          cy.get('h1').should('have.text', 'Builds for ' + userName)
-        })
+  it('Verify redirected to the “Builds for User" page', () => {
+    cy.get('div.login.page-header__hyperlinks button.jenkins-menu-dropdown-chevron').click({force:true})
+    cy.get('li.yuimenuitem a span').contains('Builds').click()
+    
+    cy.get('a.model-link span.hidden-xs.hidden-sm').then(($span) => {
+        const userName = $span.text()
+        cy.url().should("contain", "/user/" + userName + "/builds")
+        cy.get('h1').should('have.text', 'Builds for ' + userName)
+    });
   }); 
 });
