e222467bbc3805260da87040c042920b2fe3ff22
Halyna A <125617798+7108581@users.noreply.github.com>
2023-05-16 15:20:28 -0700

    My view open selected item (#244)
    * My view open selected item
    
    * changing fileName
    
    * rename file

---
+++ cypress/e2e/breadcrumbsMyViewsPage.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress"/>
+
+let userDropdown = '.jenkins-menu-dropdown-chevron:nth-child(3)';
+ 
+describe("breadcrumbsMyViewsPage", () => {
+    it('04.03_001 Verify that user can open selected Pipeline', () => {
+
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('#name').type('testPipeline');
+        cy.get('.org_jenkinsci_plugins_workflow_job_WorkflowJob').click({ force: true });
+        cy.get('#ok-button').click();
+        cy.get('.jenkins-button--primary').click();
+
+        cy.get('#breadcrumbBar li:first-child').click();
+        cy.get(userDropdown).click({ force: true })
+        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('a[href="job/testPipeline/"]').click()
+        cy.get('#main-panel > h1').should('be.visible').should('include.text', "testPipeline")
+    })
+    
+    it('04.03_002 Verify that user can open selected Folder', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('#name').type('testFolder');
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click({ force: true });
+        cy.get('#ok-button').click();
+        cy.get('.jenkins-button--primary').click();
+
+        cy.get(userDropdown).click({ force: true })
+        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('a[href="job/testFolder/"]').click()
+        cy.get('#main-panel > h1').should('be.visible')
+        cy.get('#main-panel > h1').should('include.text', "testFolder")
+    })
+});
+
