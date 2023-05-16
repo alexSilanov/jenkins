e887403b75f661b6e414a3b99952886b3992367f
Eugene <57349860+Eugenezzz@users.noreply.github.com>
2023-05-16 04:38:10 -0400

    AT_01.01_012 | Verify Jenkins Header logo (#219)

--- cypress/e2e/headerHeadIcon.cy.js
+++ cypress/e2e/headerHeadIcon.cy.js
@@ -6,4 +6,10 @@ describe('Header Head Icon', () => {
         cy.get('#jenkins-home-link').should('be.visible').click()
         cy.url().should('include', 'http://localhost:8080/')
     })
+
+    it('AT_01.01_012 | Jenkins Header logo is visible and clickable', () => {
+        cy.get('a[href="newJob"]').find('span').contains('Create a job').click()
+        cy.get('#jenkins-name-icon').click()
+        cy.url().should('eq', `http://localhost:${Cypress.env('local.port')}/`)
+    })
 })
