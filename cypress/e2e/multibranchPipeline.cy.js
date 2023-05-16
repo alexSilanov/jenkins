cc43764e01748defac3b16a18c8c009b7654f9bb
Valentina <tina.uazp@gmail.com>
2023-05-15 21:46:36 -0700

    refactore code

--- cypress/e2e/multibranchPipeline.cy.js
+++ cypress/e2e/multibranchPipeline.cy.js
@@ -1,12 +1,16 @@
 ///<reference types="cypress"/>
 const name = "Test";
 
-describe('US_16.01 | Multibranch Pipeline', () => {
-    it('AT_16.01.002 | Display Name and Description are saved', () => {
+describe('Multibranch Pipeline', () => {
+
+    beforeEach('Create Multibranch Pipeline', () => {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('#name').type(name);
         cy.contains('Multibranch Pipeline').click();
         cy.get('#ok-button').click();
+    });
+    
+    it('AT_16.01.002 | Display Name and Description are saved', () => {        
         cy.get('input[name="_.displayNameOrNull"]').click().type(name + "1");
         cy.get('textarea[name="_.description"]').click().type(name +"2");
         cy.get('button[name="Submit"]').click();
