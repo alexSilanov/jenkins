b6835b54349f658fe9a29a2ac5115b96a2644080
Vera Fes <verafes@gmail.com>
2023-05-16 15:31:11 -0700

    AT_16.03_002|<Multibranch Pipeline>Delete MultibranchPipeline from left side menu (#250)

--- cypress/e2e/multibranchPipelineDelete.cy.js
+++ cypress/e2e/multibranchPipelineDelete.cy.js
@@ -3,7 +3,7 @@
 import projects from "../fixtures/projects.json"
 import messages from "../fixtures/messages.json"
 
-describe('Multibranch Pipeline Delete Multibranch Pipeline', () => {
+describe('MultibranchPipelineDelete', () => {
 
     it('Delete the Multibranch Pipeline using dropdown menu', () => {
         cy.get('a[href="newJob"]').click()
@@ -19,6 +19,23 @@ describe('Multibranch Pipeline Delete Multibranch Pipeline', () => {
 
         cy.get('#search-box').type(projects.multibranchPipeline.name + '{enter}')
 
+        cy.get('.error').should('have.text', messages.error)  
+    });
+
+    it('AT_16.03_002_Delete the Multibranch Pipeline using left side menu', () => {
+        cy.get('a[href="newJob"]').click()
+        cy.get('input#name').type(projects.multibranchPipeline.name)
+        cy.get('li[tabindex="0"] span').contains('Multibranch Pipeline').click()
+        cy.get('#ok-button').click()
+        cy.get('#jenkins-head-icon').click()
+
+        cy.get('tbody tr td a.jenkins-table__link').click()
+        cy.get('#side-panel a[href*="delete"]').click()
+        cy.get('[name="Submit"]').click()
+
+        cy.get('#main-panel h1').should('be.visible').should('have.text', 'Welcome to Jenkins!')
+
+        cy.get('#search-box').type(projects.multibranchPipeline.name + '{enter}')
         cy.get('.error').should('have.text', messages.error)  
     });
 });
