310425c87258cca9c6ce522a22c2b275ef64f475
Olga-Khliupina <115127136+Olga-Khliupina@users.noreply.github.com>
2023-05-16 21:40:19 -0500

    AT_01.07_01 <Header> Credentials menu (#255)

--- cypress/e2e/credentialsSubmenu.cy.js
+++ cypress/e2e/credentialsSubmenu.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress"/>
 
-describe('Credentials menu link', () => {
+describe('credentialsSubmenu', () => {
 
-    it('AT_01.07_01 | <Header> Credentials menu redirects to the corresponding page', () => {
+    it('AT_01.07_01 <Header> Credentials menu redirects to the corresponding page', () => {
         cy.get('#page-header .model-link').realHover();
         cy.get('.page-header__hyperlinks .jenkins-menu-dropdown-chevron').click();
         cy.get('.yuimenuitem[index="3"]').contains('Credentials').click();
         cy.url().should('contain', 'credentials');
+        cy.get('h1').contains('Credentials');
     })
-
 })
