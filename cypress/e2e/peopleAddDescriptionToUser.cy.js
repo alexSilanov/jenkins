5744a0d996539db226c995f4366faa94595579d8
Valentin Kushnarev <121609422+valentin-kushnarev@users.noreply.github.com>
2023-05-18 00:56:09 +0400

    AT_06.02_002 | Verify description (#277)

--- cypress/e2e/peopleAddDescriptionToUser.cy.js
+++ cypress/e2e/peopleAddDescriptionToUser.cy.js
@@ -10,4 +10,15 @@ describe("peopleAddDescriptionToUser", () => {
     cy.get(".jenkins-button").click();
     cy.get("#description").should("include.text", userDescription.textDescription)
   });
+
+  it("AT_06.02_002 | Verify description is added to a user", function () {
+    cy.get("a[href='/asynchPeople/']").click()
+    cy.url().should("include", "/asynchPeople/")
+    cy.get(".jenkins-table__link").click()
+    cy.url().should("include", "/user/")
+    cy.get("#description-link").click()
+    cy.get("textarea[name='description']").clear().type(userDescription.description)
+    cy.get("button[name='Submit']").should("include.text", "Save").click()
+    cy.get("#description").should("include.text", userDescription.description)
+  })
 });
