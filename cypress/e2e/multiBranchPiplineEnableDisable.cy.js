443a5f1f1cbb3bdc8b5bf5cb9284f7bdfcef056b
OlgaAbdu <103216584+OlgaAbdu@users.noreply.github.com>
2023-05-17 14:48:43 -0500

    RF_AT_16.01.003_Enables_and_disables_the_current_Multibranch_Pipeline (#292)

--- cypress/e2e/multiBranchPiplineEnableDisable.cy.js
+++ cypress/e2e/multiBranchPiplineEnableDisable.cy.js
@@ -2,8 +2,8 @@
 const { getRandomNumber  ,createMultibranchPipeline } = require('../support/helper');
 import multibranchPipelineData from '../fixtures/multibranchPipeline.json';
 
-describe('US_16.01 Enable or disable the current Multibranch Pipeline', () => {
-    it('TC_16.01.003 Enables and disables the current Multibranch Pipeline', () => {
+describe('multiBranchPiplineEnableDisable', () => {
+    it('AT_16.01.003 Disables the current Multibranch Pipeline', () => {
       const jobType = 'Multibranch Pipeline'
       createMultibranchPipeline('.task:first-child', 'input#name', '[id="j-add-item-type-nested-projects"]', '#ok-button',  getRandomNumber(),jobType);
       cy.get('#enable-disable-project').click({ force: true });
