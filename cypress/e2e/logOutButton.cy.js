4fa001dfef9b932d97005b2aa07c578f84398d39
leshajerm <60453864+leshajerm@users.noreply.github.com>
2023-05-16 21:09:07 -0400

    01.08.12 | Header Log out button (#253)

--- cypress/e2e/logOutButton.cy.js
+++ cypress/e2e/logOutButton.cy.js
@@ -2,11 +2,18 @@
 
 describe('<Header> log out button', () => {
     it('Verify presence of log out button', function () {
-        cy.get('div[class="login page-header__hyperlinks"] :nth-child(4)').should('have.text', 'log out')      
+        cy.get('div[class="login page-header__hyperlinks"] :nth-child(4)').should('have.text', 'log out')
     });
 
     it('Verify logout button is visible and redirects to the login page', () => {
         cy.get('a[href="/logout"]').should('be.visible').click()
         cy.get('#loginIntroDefault').should('be.visible')
+    })
+
+    it('AT_01.08.12 | Header Log out button', () => {
+        cy.get('a[href="/logout" ]')
+            .should('be.visible')
+            .click()
+        cy.get('h1').should('have.text', 'Welcome to Jenkins!')
     })
 })
