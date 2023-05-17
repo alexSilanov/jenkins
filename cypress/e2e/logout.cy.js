14a242eeaf914ae0f4ec9cd2296444487fc8ad6a
Aly <65147059+Albina-star@users.noreply.github.com>
2023-05-16 22:10:12 -0400

    AT_01.08 _001| Header > Log out button >Log out button transfer the user back to the login page 'Welcome to Jenkins!' (#197)
    * Log out button transfer user back to login page
    
    * fixture added

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,7 +1,14 @@
+import logInPage from "../fixtures/logInPage.json";
+
 describe('verification log out button', function () {
 
     it('01_08_001 log out button should be clickable and open login page', function () {
         cy.get('a[href="/logout"]').should('be.visible').click()
         cy.url().should('contain','http://localhost:8080/login')
     });
+
+    it('Verify that the Log out button transfer the user back to the login page "Welcome to Jenkins!"', () => {
+        cy.get('a[href="/logout"]').should('be.visible').click()
+        cy.get('#loginIntroDefault').should('have.text', logInPage.loginPageHeader)
+    })
 });
