059db82c089e2d4b722564636487e1da95a928d6
Diana Taly <75766258+DiaTaly@users.noreply.github.com>
2023-05-17 13:20:16 -0700

    Dt/new item page items (#290)
    * new spec: New Item page - list of items
    
    * AT_05.08_005 | <New Item> New Item page displays specified list of items to be created
    
    * removed newItemListOfItems.cy.js

--- cypress/e2e/newItem.cy.js
+++ cypress/e2e/newItem.cy.js
@@ -9,4 +9,14 @@ describe('US_05.08 New Item Names and Icons', () => {
             })
             .should('deep.equal', newItemNames.projectNames)
     })
+
+    it('AT_05.08_005 | <New Item> New Item page displays specified list of items to be created', () => {
+        cy.get('a[href= "/view/all/newJob"]').click();
+        cy.get('.j-item-options li span')
+            .should('have.length', newItemNames.projectNames.length)
+            .then(($els) => {
+                return Cypress.$.makeArray($els).map($el => $el.innerText)
+            })
+            .should('deep.equal', newItemNames.projectNames)
+    })
 })
