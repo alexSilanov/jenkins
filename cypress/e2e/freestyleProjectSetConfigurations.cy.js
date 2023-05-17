43f0ef307742fb39da267e87e7e3b062da0f789b
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2023-05-17 06:56:52 -0400

    RF_12.05_002 | Freestyle project > Configure > Apply configurations changes (#256)
    * RF_12.05_002 | Freestyle project > Configure > Apply configurations changes

--- cypress/e2e/freestyleProjectSetConfigurations.cy.js
+++ cypress/e2e/freestyleProjectSetConfigurations.cy.js
@@ -1,119 +1,144 @@
 /// <reference types="cypress"/>
+
 import data from "../fixtures/freestyleBuildConfigurations.json"
 
-const createProjectWithDefaultSettings = (projectName, projectType) => {
-    cy.get('a[href="newJob"] .trailing-icon').click();
-    cy.get('#name').type(`${projectName}`);
-    cy.get('.label').contains(projectType).click();
-    cy.get('#ok-button').click();
-    goToDashboard();
-    cy.get(`#job_${projectName}`)
-        .should("have.attr", "class", " job-status-nobuilt")
-        .should("contain.text", projectName);
-    goToConfigurationsPage(data.projectName);
-    getProjectEnabled().should("have.attr", "value", "true");
-    getSourceCodeManagement().should("have.text", data.sourceCodeManagement);
-}
-
-const goToDashboard = () => cy.get('#breadcrumbs a').contains('Dashboard').click();
-const goToProjectPage = (projectName) => {
-    goToDashboard();
-    cy.get(`#job_${data.projectName} a span`).contains(`${projectName}`).click();
-}
-const goToConfigurationsPage = (projectName) => {
-    goToDashboard();
-    cy.get(`#job_${projectName} a`).realHover({ position: "center" })
-        .get(`#job_${projectName} button`).should('be.visible').click();
-
-    cy.get('#breadcrumb-menu span').contains('Configure').click()
-}
-
-const getProjectEnabled = () => cy.get('#enable-disable-project');
-
-const getSourceCodeManagement = () => {
-    cy.get("#source-code-management").should("be.visible")
-    return cy.get('label[for="radio-block-0"]');
-}
-
-const getDescription = () => cy.get('textarea[name="description"]').should("be.visible");
-
-const clickDiscardOldBuilds = () => cy.get('#cb4 + label').contains("Discard old builds").click();
-
-const getDiscardOldBuildsCheck = () => cy.get('input#cb4');
-
-const getMaxNumberOfBuildsToKeep = () => {
-    cy.get("div.form-container.tr").should("have.attr", "style", "")
-        .get('div[ref="cb4"] ~ div.form-container.tr[style] select.dropdownList option')
-        .should("be.visible").should("have.text", data.strategyOption);
-    return cy.get('input[name="_.numToKeepStr"]');
-}
-
-const getStrategy = () => cy.get('select.dropdownList option[selected="true"]');
-
-const getMaxNumberOfBuildsToKeepValue = () => cy.get('input[name="_.numToKeepStr"]');
-
-const getBuildTriggersOption = (option) => cy.get("#cb17 + label").should("be.visible").contains(option);
-
-const getBuildTriggersCheck = () => cy.get('input#cb17');
-
-const getSchedule = () => cy.get('textarea[name="_.spec"]').should("be.visible");
-
-const getBuildEnvironmentOption = (option) => cy.get('div[ref="cb23"] span.jenkins-checkbox label').should("be.visible");
-
-const getAddTimestampsCheck = () => cy.get('input#cb23');
-
-const getAddBuildSteps = () => cy.get('#build-steps + div button').contains("Add build step").should("be.visible");
-
-const getBuildStepName = () => cy.get('.repeated-chunk .repeated-chunk__header');
-
-const getScriptOption = (option) => cy.get('.bd').should("be.visible").get("a.yuimenuitemlabel").contains(option);
-
-const getScriptTextArea = () => cy.get('.CodeMirror textarea').should("be.visible");
-
-const getScriptText = () => cy.get('.CodeMirror-lines div div[style] pre');
-
-const clickApplyButton = () =>  cy.get('button[name="Apply"]').should("be.visible").click();
-const getPopUpNotification = () => cy.get('#notification-bar span');
-const getProjectDescription = () => cy.get('#description div').not('div[class]');
-
-describe('Freestyle project, Set configurations', () => {
-
-    beforeEach('Create new freestyle project with default configurations', () => {
-        createProjectWithDefaultSettings(data.projectName, data.projectType);
+describe('freestyleProjectSetConfigurations', () => {
+    Cypress.Commands.add("openDashboard", () => {
+        cy.get('#breadcrumbBar a').contains('Dashboard').click()
+            .then(() => {
+                cy.get('body')
+                    .then(($body) => {
+                        if ($body.find('.dashboard').length) {
+                            cy.get('#projectstatus').should('exist');
+                        } else {
+                            cy.get('h1').should("have.text", "Welcome to Jenkins!");
+                        }
+                    })
+            })
     })
-
-    it('AT_12.05_002 | Apply configurations changes',  () => {
-        goToConfigurationsPage(data.projectName);
-
-        getDescription().type(data.projectDescription);
-        clickDiscardOldBuilds();
-        getMaxNumberOfBuildsToKeep().type(data.maxBuilds.toString());
-        getBuildTriggersOption(data.buildTriggers).click();
-        getSchedule().type(data.schedule);
-        getBuildEnvironmentOption(data.buildEnvironmentOption).click();
-        getAddBuildSteps().click();
-        getScriptOption(data.scriptOption).click();
-        getScriptTextArea().type(data.scriptText, {force: true});
-        clickApplyButton();
-
-        getPopUpNotification().should('have.text', data.applyConfirmMessage);
-
-        goToProjectPage(data.projectName);
-
-        getProjectDescription().should("have.text", data.projectDescription);
-
-        goToConfigurationsPage(data.projectName);
-
-        getProjectEnabled().should("have.attr", "value", "true");
-        getDescription().should("have.text", data.projectDescription);
-        getDiscardOldBuildsCheck().should('have.attr', 'checked', "checked");
-        getStrategy().should("have.text", data.strategyOption);
-        getMaxNumberOfBuildsToKeepValue().should("have.attr", "value", data.maxBuilds.toString());
-        getSourceCodeManagement().should("have.text", data.sourceCodeManagement);
-        getBuildTriggersCheck().should('have.attr', 'checked', "checked");
-        getSchedule().should("have.text", data.schedule);
-        getAddTimestampsCheck().should('have.attr', 'checked', "checked");
-        getBuildStepName().should("contain.text", data.scriptOption);
-        getScriptText().should("have.text", data.scriptText);
+    Cypress.Commands.add('openConfigurationsPage', (projectName) => {
+        cy.openDashboard()
+            .then(() => {
+                cy.get(`#job_${projectName} a`).realHover({position: "center"})
+                    .then(() => {
+                        cy.get(`#job_${projectName} button`).should('be.visible').click();
+                        cy.get('#breadcrumb-menu span').contains('Configure').click()
+                            .then(() => {
+                                cy.get('#general').should("be.visible");
+                            })
+                    })
+            })
+    })
+    Cypress.Commands.add('openProjectPage', (projectName) => {
+        cy.openDashboard();
+        cy.get(`#job_${projectName} a span`).contains(`${projectName}`).click();
+    })
+    Cypress.Commands.add('createProjectWithDefaultSettings', (projectName, projectType) => {
+        cy.get('a[href="newJob"] .trailing-icon').click()
+            .then(() => {
+                cy.get('#name').type(`${projectName}`);
+                cy.get('.label').contains(projectType).click();
+                cy.get('#ok-button').click()
+                    .then(() => {
+                        cy.openDashboard()
+                            .then(() => {
+                                cy.get(`#job_${projectName}`)
+                                    .should("have.attr", "class", " job-status-nobuilt")
+                                    .should("contain.text", projectName)
+                            })
+                    })
+            });
     });
-});
+
+    const projectEnabled = "#enable-disable-project";
+    const description = "textarea[name='description']";
+    const discardOldBuildsCheck = "input#cb4";
+    const discardOldBuildsLabel = "#cb4 + label";
+    const discardOldBuildsDropDown = "div[ref='cb4'] ~ div.form-container.tr[style] select.dropdownList option";
+    const strategy = "select.dropdownList option[selected='true']";
+    const maxNumberOfBuildsToKeep = "input[name='_.numToKeepStr']";
+    const maxNumberOfBuildsToKeepValue = "input[name='_.numToKeepStr']";
+    const buildTriggersCheck = "input#cb17";
+    const buildTriggersOption = "#cb17 + label";
+    const schedule = "textarea[name='_.spec']";
+    const addTimestampsCheck = "input[name='hudson-plugins-timestamper-TimestamperBuildWrapper']";
+    const buildEnvironmentOption = "span input[name='hudson-plugins-timestamper-TimestamperBuildWrapper'] + label";
+    const addBuildStepsButton = "#build-steps + div button";
+    const scriptOptions = "#build-steps + .tr .bd";
+    const scriptOption = "a.yuimenuitemlabel";
+    const buildStepName = ".repeated-chunk .repeated-chunk__header";
+    const scriptText = ".CodeMirror-lines div div[style] pre";
+
+    it('AT_12.05_002 | Freestyle project > Configure > Apply configurations changes', () => {
+        cy.createProjectWithDefaultSettings(data.projectName, data.projectType)
+            .then(() => {
+                cy.openConfigurationsPage(data.projectName)
+                    .then(() => {
+                        cy.get(description).type(data.projectDescription);
+                        cy.get(discardOldBuildsLabel).click()
+                            .then(() => {
+                                cy.get(discardOldBuildsDropDown).should("be.visible")
+                                cy.get(maxNumberOfBuildsToKeep).type(data.maxBuilds.toString());
+                            })
+                    })
+                    .then(() => {
+                        cy.get(buildTriggersOption).should("be.visible").click();
+                        cy.get(schedule).should("be.visible").type(data.schedule);
+                    })
+                    .then(() => {
+                        cy.get('#build-environment').should("be.visible")
+                            .then(() => {
+                                cy.get(buildEnvironmentOption).contains(data.buildEnvironmentOption)
+                                    .should("be.visible")
+                                    .click();
+                            })
+                    })
+                    .then(() => {
+                        cy.get(addBuildStepsButton).should("be.visible").click();
+                        cy.get(scriptOptions).should("be.visible")
+                            .then(() => {
+                                cy.get(scriptOption).contains(data.scriptOption).click()
+                                    .then(() => {
+                                        cy.get('.CodeMirror textarea')
+                                            .should("be.visible")
+                                            .type(data.scriptText, {force: true});
+                                    })
+                            })
+                    })
+                    .then(() => {
+                        cy.get('button[name="Apply"]').should("be.visible").click()
+                            .then(() => {
+                                cy.get('#notification-bar span').should('have.text', data.applyConfirmMessage);
+                            })
+                    })
+
+                    .then(() => {
+                        cy.openDashboard();
+                        cy.openProjectPage(data.projectName)
+                            .then(() => {
+                                cy.get('#description div').not('div[class]').should("have.text", data.projectDescription);
+                            })
+                            .then(() => {
+                                cy.openConfigurationsPage(data.projectName)
+                                    .then(() => {
+                                        cy.get(projectEnabled).should("have.attr", "value", "true");
+                                        cy.get(description).should("have.text", data.projectDescription);
+                                        cy.get(discardOldBuildsCheck).should('have.attr', 'checked', "checked");
+                                        cy.get(strategy).should("have.text", data.strategyOption);
+                                        cy.get(maxNumberOfBuildsToKeepValue)
+                                            .should("have.attr", "value", data.maxBuilds.toString());
+                                        cy.get("#source-code-management").should("be.visible");
+                                        cy.get('label[for="radio-block-0"]')
+                                            .should("be.visible")
+                                            .should("have.text", data.sourceCodeManagement);
+                                        cy.get(buildTriggersCheck).should('have.attr', 'checked', "checked");
+                                        cy.get(schedule).should("have.text", data.schedule);
+                                        cy.get(addTimestampsCheck).should('have.attr', 'checked', "checked");
+                                        cy.get(buildStepName).should("contain.text", data.scriptOption);
+                                        cy.get(scriptText).should("have.text", data.scriptText);
+                                    })
+                            })
+                    })
+            })
+    })
+})
