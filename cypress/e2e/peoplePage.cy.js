657603754b1e8ef6ab0e5c38c40a5512376df57f
MarinaMikhailova <120079100+MarinaMikhailova@users.noreply.github.com>
2023-05-18 12:10:57 -0700

    AT_06.01_006 | <People>Verify redirection (#341)

--- cypress/e2e/peoplePage.cy.js
+++ cypress/e2e/peoplePage.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import pagePeopleText from "../fixtures/pagePeopleText.json"
 
 describe("People page", () => {
     it("AT_6.01_002 | People page tab", () => {
@@ -6,4 +7,9 @@ describe("People page", () => {
         cy.get(".jenkins-app-bar__content").should("contain", "People")
         cy.url().should("contain", "/asynchPeople/")
     });
+
+    it('Verify redirection to People page', () => {
+        cy.get('a[href="/asynchPeople/"]').click();
+        cy.get('.jenkins-app-bar__content').should('have.text', pagePeopleText.textPeoplePage);
+    })
 });
