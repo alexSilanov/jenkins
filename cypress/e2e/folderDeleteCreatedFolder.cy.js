c0844d0f80970e6e64cff7c7d0759655cc950de7
IrynaLyakhova <irynalyakhova@gmail.com>
2023-05-18 13:28:02 -0700

    refactorTest

--- cypress/e2e/folderDeleteCreatedFolder.cy.js
+++ cypress/e2e/folderDeleteCreatedFolder.cy.js
@@ -1,17 +1,16 @@
 describe('Folder | Delete created folder', () => {
     it('AT_15.04.004 | Folder | Delete created folder', () => {
-
+        const newFolder = 'newFolder'
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.get('#name').type('newFolder')
+        cy.get('#name').type(newFolder)
         cy.get('span.label').contains('Folder').click()
         cy.get('#ok-button').click()
         cy.get('#bottom-sticker').contains('Save').click()
 
-        cy.url().should('include', '/job/newFolder/')
+        cy.url().should('include', `/job/${newFolder}/`)
         cy.get('span.task-link-text').contains('Delete Folder').click({ force: true })
         cy.get('#main-panel button[name="Submit"]').contains('Yes').click()
-        cy.url('http://localhost:8080/')
-
+        cy.get('.empty-state-block').should('not.have.value', newFolder)
     })
 })
 
