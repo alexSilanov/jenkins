42e98618cfc0a36628786beb3f07394a5a2f2c0e
SiarheiMSV <106940126+SiarheiMSV@users.noreply.github.com>
2023-05-17 21:27:27 -0400

    AT_20.03_001 | Verify names of items of Freestylep (#304)

---
+++ cypress/e2e/dashboardFreestyleDropdownMenu.cy.js
@@ -0,0 +1,26 @@
+/// <reference types='cypress' />
+
+import freestyleDropdownItems from "../fixtures/freestyleDropdownMenu.json"
+
+describe('dashboardFreestyleDropdownMenu', () => {
+
+    beforeEach("Creat new Freestyle project", () => {
+        cy.get('a[href="newJob"]').click();
+        cy.get('.add-item-name .jenkins-input').type('NewFProject')
+        cy.get('.hudson_model_FreeStyleProject').click()
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('#jenkins-home-link').click()
+    })
+
+    it('AT_20.03_001 | Verify names of items of Freestyle projects name hoverable dropdown menu', () => {
+        cy.get('.jenkins-table__link.model-link.inside button').click({ force: true })
+        cy.get('.first-of-type li').then($el => {
+            let array = Cypress.$.makeArray($el).map($el => $el.innerText);
+            expect(array).to.be.deep.equal(freestyleDropdownItems.freestyleDropdownItems);
+        })
+    })
+    
+})
+
+
