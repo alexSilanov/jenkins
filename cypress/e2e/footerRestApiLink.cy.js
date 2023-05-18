560aab50b5550e146e15d7eede8971a1af2db4d1
AnastasiiaMysiuk <89641258+AnastasiiaMysiuk@users.noreply.github.com>
2023-05-18 20:03:14 +0200

    AT_03.01_007|Verify the clickability of a REST API button (#332)
    * AT_03.01_007|Verify the clickability of a REST API button
    
    * Changed url to css selector
    
    * Changed test name

--- cypress/e2e/footerRestApiLink.cy.js
+++ cypress/e2e/footerRestApiLink.cy.js
@@ -5,4 +5,9 @@ describe('footerRestApiLink', () => {
         cy.get('a[href="api/"]').click();
         cy.get('a[href="/api/"]').should('contain.text', 'API');
     });
+
+    it('AT_03.01_007 | Footer | Verify the clickability of a REST API button', () =>{
+        cy.get('.page-footer__links a[href="api/').click();
+        cy.get('#main-panel h1').should('have.text', 'REST API');
+    })
 });
