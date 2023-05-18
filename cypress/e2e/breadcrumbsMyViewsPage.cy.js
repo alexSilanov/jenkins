ccd0380da8b050156c2927c21df31c120b71320e
Halyna A <125617798+7108581@users.noreply.github.com>
2023-05-18 13:18:50 -0700

    04.03_007 VerifyUserCanOpen Multi-configurProject (#340)

--- cypress/e2e/breadcrumbsMyViewsPage.cy.js
+++ cypress/e2e/breadcrumbsMyViewsPage.cy.js
@@ -7,7 +7,7 @@ describe("breadcrumbsMyViewsPage", () => {
 
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('#name').type('testPipeline');
-        cy.get('.org_jenkinsci_plugins_workflow_job_WorkflowJob').click({ force: true });
+        cy.get('.org_jenkinsci_plugins_workflow_job_WorkflowJob').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
@@ -15,35 +15,53 @@ describe("breadcrumbsMyViewsPage", () => {
         cy.get(userDropdown).click({ force: true })
         cy.get('.yuimenuitem').contains("My Views").click()
         cy.get('a[href="job/testPipeline/"]').click()
-        cy.get('#main-panel > h1').should('be.visible').should('include.text', "testPipeline")
+        cy.get('#main-panel > h1')
+          .should('be.visible')
+          .and('include.text', "testPipeline")
     })
     
     it('04.03_002 Verify that user can open selected Folder', () => {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('#name').type('testFolder');
-        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click({ force: true });
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
         cy.get(userDropdown).click({ force: true })
         cy.get('.yuimenuitem').contains("My Views").click()
         cy.get('a[href="job/testFolder/"]').click()
-        cy.get('#main-panel > h1').should('be.visible')
-        cy.get('#main-panel > h1').should('include.text', "testFolder")
+        cy.get('#main-panel > h1')
+          .should('be.visible')
+          .and('include.text', "testFolder")
     })
 
     it('04.03_003 Verify that user can open selected Freestyle project', () => {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('#name').type('testFreestyleProject');
-        cy.get('.hudson_model_FreeStyleProject').click({ force: true });
+        cy.get('.hudson_model_FreeStyleProject').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
         cy.get(userDropdown).click({ force: true })
         cy.get('.yuimenuitem').contains("My Views").click()
         cy.get('#job_testFreestyleProject').click()
-        cy.get('a[class="jenkins-table__link model-link inside"]').should('be.visible')
-        cy.get('a[class="jenkins-table__link model-link inside"]').should('include.text', "testFreestyleProject")
+        cy.get('a[class="jenkins-table__link model-link inside"]')
+          .should('be.visible')
+          .and('include.text', "testFreestyleProject")
+    })
+    it('04.03_007 Verify that user can open selected Multi-configuration project', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('#name').type('testMulti Configuration Project');
+        cy.get('.hudson_matrix_MatrixProject').click();
+        cy.get('#ok-button').click();
+        cy.get('.jenkins-button--primary').click();
+
+        cy.get(userDropdown).click({ force: true })
+        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('a[class="jenkins-table__link model-link inside"] span').click()
+        cy.get('h1[class="matrix-project-headline page-headline"]')
+          .should('be.visible')
+          .and('include.text', "testMulti Configuration Project")
     })
 });
 
