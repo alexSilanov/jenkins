7af7939a8bbbbaaaeeaa35d0b304a7e6c4a95547
MarinaMikhailova <120079100+MarinaMikhailova@users.noreply.github.com>
2023-05-18 08:37:24 -0700

    AT_06.02_003 | Verify save button (#315)

--- cypress/e2e/peopleAddDescriptionToUser.cy.js
+++ cypress/e2e/peopleAddDescriptionToUser.cy.js
@@ -2,6 +2,8 @@
 import userDescription from "../fixtures/userDescription.json";
 
 describe("peopleAddDescriptionToUser", () => {
+  const newDescription = 'new user description';
+
   it("AT 06.02.001 | Verify description is added to user", function () {
     cy.get("a.task-link").eq(1).click();
     cy.get(".jenkins-table__link").first().click();
@@ -11,6 +13,17 @@ describe("peopleAddDescriptionToUser", () => {
     cy.get("#description").should("include.text", userDescription.textDescription)
   });
 
+  it('AT_06.02_003 | Verify save button functionality', () => {
+    cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
+    cy.get('.jenkins-table__link').click();
+    cy.get('#description-link').click();
+    cy.get('.jenkins-input').type(newDescription);
+    cy.get('.jenkins-button').click();
+    cy.get(':nth-child(1) > .task-link-wrapper > .task-link').click();
+    cy.get('.jenkins-table__link').click();
+    cy.get('#description').should('include.text','new user description')
+    
+  });
   it("AT_06.02_002 | Verify description is added to a user", function () {
     cy.get("a[href='/asynchPeople/']").click()
     cy.url().should("include", "/asynchPeople/")
