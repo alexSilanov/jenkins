04522c6f03ef759d9435f00be1a052798b5c210f
Aleksandra Folkes <124165961+SashaFoxx@users.noreply.github.com>
2023-05-17 22:38:44 -0400

    AT_20.04_002 Verify Dropdown menu is visible (#308)

--- cypress/e2e/pipelineDropdownMenu.cy.js
+++ cypress/e2e/pipelineDropdownMenu.cy.js
@@ -9,15 +9,23 @@ describe('New item Create a new Pipeline', () => {
         cy.contains('Pipeline').click();
         cy.get('#ok-button').click();
         cy.get('button[name="Submit"]').click();
+        cy.contains('Dashboard').click();
     });
 
     it("AT_20.04 | <Dashboard>Jenkins Table:: Pipeline's name hoverable dropdown menu", () => {
-        cy.contains('Dashboard').click();
         cy.get('.jenkins-table__link.model-link.inside button').realHover().realClick();
         cy.get('.first-of-type li').then($el => {
             let arr = Cypress.$.makeArray($el).map($el => $el.innerText);
             expect(arr.length).to.be.equal(7);
             expect(arr).to.be.deep.equal(pipelineDropdown.pipelineDropdownItems);
+        })
+     
+    })
+
+    it("AT_20.04_002| Verify Dropdown menu is visible", () => {
+        cy.get('#projectstatus tbody button').realHover().click();
+        cy.get('.first-of-type span').should('be.visible').and('have.length', 7).each(($el, idx) => {
+            expect($el.text()).to.be.equal(pipelineDropdown.pipelineDropdownItems[idx])
         })
     })
 })
