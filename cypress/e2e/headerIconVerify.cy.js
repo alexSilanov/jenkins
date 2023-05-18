feb9eb24fee18d1d06e698ad7ce34a390ca9b5fb
Mykhailo Taran <mtaran.job@gmail.com>
2023-05-18 20:23:46 +0300

    AT_01.01.032  Header Icon verify (#265)

--- cypress/e2e/headerIconVerify.cy.js
+++ cypress/e2e/headerIconVerify.cy.js
@@ -5,4 +5,8 @@ describe('Header | Head Icon', () => {
     it('Verify Jenkins icon', function () {
         cy.get('#jenkins-head-icon').should('be.visible');        
     });
+
+    it('AT_01.01.032 | Header Icon verify', () => {
+        cy.get('#jenkins-head-icon').should('be.visible')    
+    })
 })
