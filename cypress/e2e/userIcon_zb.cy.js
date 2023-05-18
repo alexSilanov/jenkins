644312520936110fe322dba690711840f5c175da
Alla Anderson <76411309+allamalitska@users.noreply.github.com>
2023-05-17 23:06:30 -0700

    AT_01.03_014| selector is changed (#314)

--- cypress/e2e/userIcon_zb.cy.js
+++ cypress/e2e/userIcon_zb.cy.js
@@ -2,8 +2,8 @@
 
 describe('Header | User icon', () => {
 
-    it('AT_01.03_013| <Header> Verify "Use Icon" is visible and clickable', () => {
-        cy.get('a[href="/user/admin"]').should('exist').click()
+    it('AT_01.03_013| <Header> Verify "User Icon" is visible and clickable', () => {
+        cy.get('.login > a.model-link').should('exist').click()
         cy.get('#main-panel h1').should('contain', 'admin')
     })
 
