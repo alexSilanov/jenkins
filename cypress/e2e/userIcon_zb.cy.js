cf90b6662270af3e3fba2efc23d240f7ba1c168b
Alla Anderson <76411309+allamalitska@users.noreply.github.com>
2023-05-18 09:07:48 -0700

    AT_01.03_014| selector is changed (#317)

--- cypress/e2e/userIcon_zb.cy.js
+++ cypress/e2e/userIcon_zb.cy.js
@@ -7,14 +7,14 @@ describe('Header | User icon', () => {
         cy.get('#main-panel h1').should('contain', 'admin')
     })
 
-    it('AT_01.03_014| <Header> Verify dropdown menu from "Use Icon"', () => {
-        cy.get('a[href="/user/admin"]>button').click({ force: true })
+    it('AT_01.03_014| <Header> Verify dropdown menu from "User Icon"', () => {
+        cy.get('.login > a.model-link > button').click({ force: true })
         cy.get('#breadcrumb-menu ul>li span')
             .should("have.length", 4).then(($elems) => {
                 const element = Cypress.$.makeArray($elems).filter($elem => $elem.innerText == 'Builds')
                 return cy.wrap(element)
             }).click()
-        cy.get('#main-panel h1').should('contain', 'Builds for admin')
+        cy.get('#breadcrumbBar>ol>li:last-child').should('have.text', 'Builds')
     })
 
 })
