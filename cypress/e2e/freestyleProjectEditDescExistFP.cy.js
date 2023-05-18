1425491f0b4026c22698ad0e1aee47ce1094d795
S-Lanchik <130118964+S-Lanchik@users.noreply.github.com>
2023-05-17 21:18:52 -0700

    AT_12.07.003 | Verify possibility to edit description (#303)

--- cypress/e2e/freestyleProjectEditDescExistFP.cy.js
+++ cypress/e2e/freestyleProjectEditDescExistFP.cy.js
@@ -14,4 +14,19 @@ describe('FreestyleProjectEditDescription', () => {
       cy.get('button[name="Submit"]').click()
       cy.get('#description').contains('Description from Home page')
     })
+
+    it('AT_12.07.003 | Freestyle project> Verify possibility to edit description', () => {
+      cy.get('a[href="newJob"]').click()
+      cy.get('input#name').type('Project name')
+      cy.get('.hudson_model_FreeStyleProject').click()
+      cy.get('#ok-button').click()
+      cy.get('[name="description"]').type('Description')
+      cy.get('[name="Submit"]').click()
+
+      cy.get('#description-link').click()
+      cy.get('[name="description"]').type('New ')
+      cy.get('.jenkins-button--primary ').click()
+
+      cy.get('#description div:first-child').should('have.text', 'New Description')
+    })
 })
