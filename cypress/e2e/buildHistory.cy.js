8309cbbb616ad8cdd215bece8bd68a1dbc834338
AnastasiiaMysiuk <89641258+AnastasiiaMysiuk@users.noreply.github.com>
2023-05-18 10:38:15 +0200

    AT_07.01_003 | <Build History> Verify date when the build was created is visible (#246)
    * Added test Verify date when the build was created is visible
    
    * Changed css selectors, removed { force: true }

--- cypress/e2e/buildHistory.cy.js
+++ cypress/e2e/buildHistory.cy.js
@@ -10,4 +10,20 @@ describe('Build History|View build history calendar', () => {
         cy.get('a[href="/view/all/builds"').click()
         cy.get('.jenkins-app-bar__content').should('have.text', 'Build History of Jenkins')
     })
+
+    it('AT_07.01_003|<Build History> Verify date when the build was created is visible', () => {
+    
+        cy.get('#side-panel [href$="newJob"]').click();
+        cy.get('.jenkins-input').type('project1')
+        cy.get("li[class='hudson_model_FreeStyleProject']").click()
+        cy.get("#ok-button").click()
+        cy.get("button[name='Submit']").click()
+        cy.get('.model-link').contains('Dashboard').click()
+        cy.get('#job_project1').should('be.visible')
+        cy.get('#job_project1 > :nth-child(7)').click()
+        cy.get('[href="/view/all/builds"').click();
+        cy.get('.jenkins-app-bar__content').should('have.text', 'Build History of Jenkins');
+        cy.get('.label-event-blue').click();
+        cy.get('.timeline-event-bubble-time').should('be.visible')
+    });
 })
