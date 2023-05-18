e5461f2cafbdfe9204b8f5cfa57dbab7726322b2
asyniagina <56331881+asyniagina@users.noreply.github.com>
2023-05-18 13:16:00 -0700

    AT_17.01.001 | Org folder | Configure | Change status folder to disable (#348)

--- cypress/e2e/orgFolderConfigurate.cy.js
+++ cypress/e2e/orgFolderConfigurate.cy.js
@@ -2,6 +2,7 @@
 describe('Organization Folder| Configurate Organization Folder', () => {
     const orgFolderName = 'OrgFolderTest';
     const description = 'The description was added to the organization folder.';
+    const disableMessage = 'This Organization Folder is currently disabled';
 
     it('AT_17.01.002| Add description to the Organization Folder via Configurate', () => {
         cy.get('a[href="/view/all/newJob"]').click();
@@ -11,10 +12,25 @@ describe('Organization Folder| Configurate Organization Folder', () => {
         cy.get('button[name="Submit"]').click();
         cy.get('#breadcrumbBar li:first-child').click();
 
-        cy.get('.jenkins-table__link.model-link.inside').click( {multiple: true} );
+        cy.get('.jenkins-table__link.model-link.inside').click();
         cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
         cy.get('#main-panel > form > div.config-table > div.jenkins-form-item.tr > div.setting-main > textarea').type(description);
         cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary').click();
         cy.get('#view-message').should('have.text', description);
+    })
+
+    it('AT_17.01.001 | Change status folder to disable', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('#name').type(orgFolderName);
+        cy.get('.jenkins_branch_OrganizationFolder').click();
+        cy.get('#ok-button').click();
+        cy.get('button[name="Submit"]').click();
+        cy.get('#breadcrumbBar li:first-child').click();
+
+        cy.get('.jenkins-table__link.model-link.inside').click();
+        cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
+        cy.get('.jenkins-toggle-switch__label').click();
+        cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary').click();
+        cy.get('#enable-project').should('contain', disableMessage);
     })
 });
