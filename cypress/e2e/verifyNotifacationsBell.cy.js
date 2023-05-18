faacaa81d80f633f216fe1e3ea1e7048d780820a
LinaAbu1306 <Lina.qa.abu@gmail.com>
2023-05-18 00:21:10 -0400

    AT_01.10.004|Verify Orange Notifications icon (#311)

--- cypress/e2e/verifyNotifacationsBell.cy.js
+++ cypress/e2e/verifyNotifacationsBell.cy.js
@@ -5,4 +5,9 @@ describe('Header | Notifications icon', () => {
         cy.get('#visible-am-button').click()
         cy.get('#visible-am-list a[href="/manage"]').should('have.text', 'Manage Jenkins')
     })
+
+    it('AT_01.10.004 | Header | Verify That Orange Notifications icon is Visible', () => {
+        cy.get('#visible-am-insertion span').realHover()
+        cy.get('#visible-am-insertion span').should('have.css','background-color','rgb(255, 152, 0)')
+    })
 })
