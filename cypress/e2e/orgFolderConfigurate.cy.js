f2ed9a6bb46931ef4d0d9e2ee6fbff8f3dcc9ed6
asyniagina <56331881+asyniagina@users.noreply.github.com>
2023-05-18 15:26:51 -0700

    [RF] orgFolderConfigurate - created function createOrgFolder (#359)

--- cypress/e2e/orgFolderConfigurate.cy.js
+++ cypress/e2e/orgFolderConfigurate.cy.js
@@ -1,36 +1,34 @@
 // <reference types=“cypress”/>
-describe('Organization Folder| Configurate Organization Folder', () => {
+
+function createOrgFolder(orgFolderName) {
+    cy.get('a[href="/view/all/newJob"]').click();
+    cy.get('#name').type(orgFolderName);
+    cy.get('.jenkins_branch_OrganizationFolder').click();
+    cy.get('#ok-button').click();
+    cy.get('button[name="Submit"]').click();
+    cy.get('#breadcrumbBar li:first-child').click();
+}
+
+describe('orgFolderConfigurate', () => {
     const orgFolderName = 'OrgFolderTest';
     const description = 'The description was added to the organization folder.';
     const disableMessage = 'This Organization Folder is currently disabled';
 
-    it('AT_17.01.002| Add description to the Organization Folder via Configurate', () => {
-        cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type(orgFolderName);
-        cy.get('.jenkins_branch_OrganizationFolder').click();
-        cy.get('#ok-button').click();
-        cy.get('button[name="Submit"]').click();
-        cy.get('#breadcrumbBar li:first-child').click();
-
+    it('AT_17.01.002 | Add description to the Organization Folder via Configure', () => {
+        createOrgFolder(orgFolderName);
         cy.get('.jenkins-table__link.model-link.inside').click();
         cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
         cy.get('#main-panel > form > div.config-table > div.jenkins-form-item.tr > div.setting-main > textarea').type(description);
         cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary').click();
         cy.get('#view-message').should('have.text', description);
-    })
+    });
 
     it('AT_17.01.001 | Change status folder to disable', () => {
-        cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type(orgFolderName);
-        cy.get('.jenkins_branch_OrganizationFolder').click();
-        cy.get('#ok-button').click();
-        cy.get('button[name="Submit"]').click();
-        cy.get('#breadcrumbBar li:first-child').click();
-
+        createOrgFolder(orgFolderName, '');
         cy.get('.jenkins-table__link.model-link.inside').click();
         cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
         cy.get('.jenkins-toggle-switch__label').click();
         cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary').click();
         cy.get('#enable-project').should('contain', disableMessage);
-    })
+    });
 });
