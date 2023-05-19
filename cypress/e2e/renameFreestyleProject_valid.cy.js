cdd424584b04640418e92b479d9ec94ce9488f33
Katerina <katerina@a0a0.ru>
2023-05-19 16:22:37 +0300

    add formatting and should

--- cypress/e2e/renameFreestyleProject_valid.cy.js
+++ cypress/e2e/renameFreestyleProject_valid.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
-describe('renameFreestyleProject_valid', () => {
+
+describe('renameFreestyleProjectValid', () => {
     let projectName = "DemoJob";
     let newProjectName = "AnotherProject";
 
@@ -10,24 +11,23 @@ describe('renameFreestyleProject_valid', () => {
       cy.get('#ok-button').should('have.text', 'OK').click();
       cy.get('.setting-main  textarea  ').eq(0).type('test');
       cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary')
-      .click();
+        .click();
 
       cy.get('.jenkins-breadcrumbs__list-item > .model-link').eq(0).click();
 
       cy.get('td> a > span').should('have.text', projectName).realHover();
       cy.get('td> a > button').should('be.visible').click();
       cy.get('#breadcrumb-menu>div.bd>ul.first-of-type>li>a>span')
-      .should('be.visible')
-      .and('have.length', 6)
-      .eq(5)
-      .click();
+        .should('be.visible')
+        .and('have.length', 6)
+        .eq(5)
+        .click();
   });
       it("AT_12.03.012 Rename project, new name is valid", () => {
-      cy.get('input[name="newName"]').clear().type(newProjectName);
-      cy.get('button[name="Submit"]').click();
+        cy.get('input[name="newName"]').clear().type(newProjectName);
+        cy.get('button[name="Submit"]').click();
 
-      cy.get('div h1').contains(newProjectName);
-      
+        cy.get('h1.job-index-headline.page-headline').should('have.text', `Project ${newProjectName}`);         
     });
 
 });
