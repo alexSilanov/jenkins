09d30e3f27e593553233907f32756c68de543c21
S-Lanchik <130118964+S-Lanchik@users.noreply.github.com>
2023-05-19 13:17:55 -0700

    AT_17.05.002 | Verify possibility to enable Organization Folder (#381)
    * AT_17.05.002 | Verify possibility to enable Organization Folder

--- cypress/e2e/orgFolderDisableEnable.cy.js
+++ cypress/e2e/orgFolderDisableEnable.cy.js
@@ -17,4 +17,16 @@ describe('Organization Folder Disable/Enable', () => {
         cy.get('#disable-project button').click()
         cy.get('form#enable-project').should('contain', projects.organizationFolder.disable)
     });
+
+    it('AT_17.05.002 | Verify possibility to enable Organization Folder', () => {
+        cy.get('#projectstatus').contains(projects.organizationFolder.name).click()
+        cy.get('#disable-project button').click()
+        cy.get('form#enable-project').should('contain', projects.organizationFolder.disable)
+
+        cy.get('#jenkins-name-icon').click()
+        cy.get('#projectstatus').contains(projects.organizationFolder.name).click()
+        cy.get('button.jenkins-button').click()
+
+        cy.get('form#disable-project').should('have.text', projects.organizationFolder.disableBtn)
+    });
 });
