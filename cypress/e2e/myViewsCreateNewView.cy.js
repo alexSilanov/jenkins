ea2722562d9edf820aecbc0433a90fa314413a05
DimaCurkanu <130791484+DimaCurkanu@users.noreply.github.com>
2023-05-19 13:23:41 -0500

    Add_beforeEach&afterEach and change fixtures (#403)

--- cypress/e2e/myViewsCreateNewView.cy.js
+++ cypress/e2e/myViewsCreateNewView.cy.js
@@ -2,7 +2,8 @@
 import createNewView from "../fixtures/createNewView.json"
 
 describe('myViewsCreateNewView', () => {
-  it('AT_09.01_001 | My Views>Create new view', function () {
+
+  beforeEach('create the job', function () {
     cy.get('a[href$="my-views"]').realHover();
     cy.get('a[href$="my-views"]').click();
     cy.get('a[href$="/newJob"]').click();
@@ -14,14 +15,19 @@ describe('myViewsCreateNewView', () => {
     cy.get('.job-index-headline').should('have.text', `Project ${createNewView.jobName}`);
     cy.get('a.model-link[href="/"').click();
     cy.get('a[href$="my-views"]').should('contain.text', createNewView.myView).click();
-      
+})
+
+  it('AT_09.01_001 | My Views>Create new view', function () {
     cy.get('.addTab').should('be.visible').click();
     cy.get('#name').type(createNewView.viewName);
     cy.get('.jenkins-radio:last-child label').should('have.text', createNewView.myView).click();
     cy.get('#ok').click();
     cy.get('.tab.active').contains(createNewView.viewName);
+  });
+
+  afterEach('delete new view', function () {
     cy.get('a[href$="delete"]').click();
     cy.get('#main-panel > form > button').click();
-  });
+  })
 })
 
