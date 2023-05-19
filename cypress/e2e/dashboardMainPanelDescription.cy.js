55d3642d198817004874c71857227577ab13fabb
nadilova <nadilova@gmail.com>
2023-05-19 08:45:33 +0200

    AT_02.06_004 | Dashboard > Description input textarea is invisible

--- cypress/e2e/dashboardMainPanelDescription.cy.js
+++ cypress/e2e/dashboardMainPanelDescription.cy.js
@@ -4,10 +4,10 @@ import descriptionsProject from "../fixtures/descriptionsProject.json"
 
 describe('dashboardMainPanelDescription', () => {
 
-    it("AT_02.06_004| Dashboard > Verification of the button 'Add description'", () => {
-        cy.get("a#description-link").click() //find and click the button 'Add description'
-        cy.focused().should('have.attr', 'name', 'description') /*assert that the currently focused DOM element 
-        is input textarea for adding description located at the main panel of the Dashboard*/
+    it("AT_02.06_004 | Dashboard > Description input textarea is invisible", () => {
+        cy.get("textarea.jenkins-input").should('not.exist')
     })
 
 })
+
+   
