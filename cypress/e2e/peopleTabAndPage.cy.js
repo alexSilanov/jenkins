8c810d5a94be3a3b3e6f2319a9472bca5c5a755f
Peri Ismailova <89989555+Parizat-Ismailova@users.noreply.github.com>
2023-05-19 13:54:43 -0500

    AT_06.01_003 | People | Verify the "People" page (#405)

--- cypress/e2e/peopleTabAndPage.cy.js
+++ cypress/e2e/peopleTabAndPage.cy.js
@@ -10,4 +10,10 @@ describe("People Tab and People page", () => {
     cy.get('a[href="/asynchPeople/"]').click()
     cy.url().should('contain', '/asynchPeople/')
   })
+
+  it('AT_06.01_003 | People | Verify the "People" page', ()=>{
+    cy.get('#side-panel #tasks :nth-child(2) .task-link-text').should('exist').and('have.text','People')
+    cy.get('a[href="/asynchPeople/"]').click()
+    cy.url().should('includes','/asynchPeople/')
+  })
 });
