0930b3dc0463f67de1ad928639d9f79ef0634b02
S-Lanchik <130118964+S-Lanchik@users.noreply.github.com>
2023-05-18 21:57:29 -0700

    AT_17.05.001 | Possibility to disable Organization Folder (#352)
    * AT_17.05.001 | Possibility to disable Org Folder

---
+++ cypress/e2e/orgFolderDisableEnable.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+import projects from '../fixtures/projects.json'
+
+describe('Organization Folder Disable/Enable', () => {
+    beforeEach(() => {
+        cy.get('a[href="newJob"]').click()
+        cy.get('input#name').type(projects.organizationFolder.name)
+        cy.get('li.jenkins_branch_OrganizationFolder').click()
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('#jenkins-name-icon').click()
+    });
+
+    it('AT_17.05.001 | Verify possibility to disable Organization Folder', () => {
+        cy.get('#projectstatus').contains(projects.organizationFolder.name).click()
+        cy.get('#disable-project button').click()
+        cy.get('form#enable-project').should('contain', projects.organizationFolder.disable)
+    });
+});
