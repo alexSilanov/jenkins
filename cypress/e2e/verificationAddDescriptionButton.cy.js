42c12ff8a2a10a8ee1bbad8ed70170b0be55759c
Anna <126493566+supremexkid@users.noreply.github.com>
2023-05-19 22:50:34 +0300

    AT_02.09_004 | Home page>Verify "add description" button (#366)
    * AT_02.09_004 | Verificate "add description" button

--- cypress/e2e/verificationAddDescriptionButton.cy.js
+++ cypress/e2e/verificationAddDescriptionButton.cy.js
@@ -5,4 +5,9 @@ describe('Home page>Verification "add description" button', () => {
         cy.get('#description-link').click()
         cy.get('#description').should('be.visible').click();        
     });
+
+    it('AT_02.09_004 | Home page>Verification "add description" button', () =>{
+        cy.get('#description-link').click()
+        cy.get('#description > form').should('be.visible');  
+    });
 });
