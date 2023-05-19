f85a09062f4a160167d094478e3a79b471b0b032
viktoriashidlovskaya <121621494+viktoriashidlovskaya@users.noreply.github.com>
2023-05-19 11:55:43 +0300

    AT_18.04_001 | Profile Page|Configure is displayed (#384)

---
+++ cypress/e2e/profilePageConfigureLink.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+const userName = Cypress.env("local.admin.username");
+
+describe ('profilePageConfigureLink', () => {
+
+    it("AT_18.04_001 | Profile Page | Link to User's configure | Configure is displayed on User's profile page", () => {
+        cy.get(`a[href="/user/${userName}"]>span`).click()
+        cy.get(`a[href="/user/${userName}/configure"]`).should("be.visible")
+        cy.get(`a[href="/user/${userName}/configure"] .task-link-text`).should("have.text", "Configure")
+    })
+})
