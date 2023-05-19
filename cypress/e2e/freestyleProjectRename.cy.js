1d53e5199be6fcaf8dca34436028f13f27b87ca2
Anzhelika Baa <115436933+AnzhelikaBaa@users.noreply.github.com>
2023-05-19 19:19:41 -0400

    AT_12.03.003| <Freestyle project> Rename project from Freestyle project page, and verify it is accessible and visible on the Jenkins dashboard under the new name. (#415)
    * AT_12.03.003| <Freestyle project> Verify the existing project was renamed from the Freestyle project page, and now is accessible and visible on the Jenkins dashboard under the new name.
    
    * AT_12.03.003| <Freestyle project> Rename project from Freestyle project page.
    
    * AT_12.03.003| <Freestyle project> Rename project from Freestyle project page,and check it is accessible and visible on the Jenkins dashboard under the new name.
    
    * AT_12.03.003| <Freestyle project> Rename project from Freestyle project page, and verify it is accessible and visible on the Jenkins dashboard under the new name.
    
    * Only removed.

--- cypress/e2e/freestyleProjectRename.cy.js
+++ cypress/e2e/freestyleProjectRename.cy.js
@@ -1,20 +1,21 @@
 /// <reference types="cypress"/>
 
 import messages from "../fixtures/messages.json"
+import freestyleProject from "../fixtures/freestyleProject.json"
 
 describe('Freestyle project - Rename project', () => {
-    
+
     let jobName = 'Project1'
-    
-    beforeEach ('Create freestyle project', function () {
+
+    beforeEach('Create freestyle project', function () {
         cy.get('a[href="newJob"]').click()
         cy.get('input#name').type(jobName)
         cy.get('li[tabindex="0"] span').contains('Freestyle project').click()
         cy.get('#ok-button').click()
         cy.get(':nth-child(1) > .model-link').click()
     });
-    
-    it ('AT_12.03_001 | Verify renaming freestyle project using dropdown menu' , function () {
+
+    it('AT_12.03_001 | Verify renaming freestyle project using dropdown menu', function () {
         cy.get('tbody tr td a.jenkins-table__link').should('be.visible').should('have.text', jobName).realHover()
         cy.get('table#projectstatus button.jenkins-menu-dropdown-chevron').should('be.visible').click()
         cy.contains('div#breadcrumb-menu ul li a', 'Rename').should('be.visible').click();
@@ -29,7 +30,7 @@ describe('Freestyle project - Rename project', () => {
         cy.get('.icon-edit-delete').click()
     })
 
-    it('AT_12.03_002 | Verify that using the same name an error message is appeared', function() {
+    it('AT_12.03_002 | Verify that using the same name an error message is appeared', function () {
         cy.get('.jenkins-table__link').click()
         cy.get('a[href$="confirm-rename"]').click()
         cy.get('.jenkins-input').should('have.value', jobName)
@@ -37,5 +38,21 @@ describe('Freestyle project - Rename project', () => {
 
         cy.get('#main-panel h1').should('have.text', messages.renameErrorMessage.error).and('be.visible')
         cy.get('#main-panel p').should('have.text', messages.renameErrorMessage.message).and('be.visible')
+    });
+
+    it('AT_12.03.003| <Freestyle project> Rename project from Freestyle project page,and verify it is accessible and visible on the Jenkins dashboard under the new name.', function () {
+        cy.get('#job_Project1 td:nth-child(3) a span')
+          .should('be.visible')
+          .and('have.text', freestyleProject.projectName).click()
+        cy.get('#main-panel h1').should('be.visible').contains(freestyleProject.projectName)
+        cy.get('a[href="/job/Project1/confirm-rename"]').click()
+        cy.get('.setting-main').click()
+        cy.get('div.setting-main input').clear().type(freestyleProject.projectNewName)
+        cy.get('#bottom-sticker div button').click()
+        cy.get('#breadcrumbs li a[href="/"]').click()
+
+        cy.get('tr td:nth-child(3) a span')
+          .should('be.visible')
+          .and('have.text', freestyleProject.projectNewName)
     });
 })
