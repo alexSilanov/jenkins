9d3ba3d48780ebb3de21302d05ad321d9649200b
SvetlanaKazakova <107031108+SvetlanaKazakova@users.noreply.github.com>
2023-05-19 13:02:46 +0300

    AT_01.06.05_ | Header My views menu link (#387)
    * Header My views menu link

--- cypress/e2e/headerMyViewMenuLinks.cy.js
+++ cypress/e2e/headerMyViewMenuLinks.cy.js
@@ -9,4 +9,12 @@ describe("US_01.06 Header My views menu link", () => {
         cy.get('@sidePanel').find('[href="/asynchPeople/"]').should('be.visible');
         cy.get('@sidePanel').find('[href$="builds"]').should('be.visible');
     })
+    it('TC_01.06.05 Header My views menu link',()=>{
+        cy.get('a[href="/user/admin"] .jenkins-menu-dropdown-chevron').click({force: true});
+        cy.get('[href="/user/admin/my-views"]').click();
+        cy.get('.content-block').should('have.text','Create a job');
+        cy.get('[href="/user/admin/my-views/view/all/newJob"]').should('have.text','New Item');
+        cy.get('[href="/asynchPeople/"] .task-link-text').should('have.text','People');
+        cy.get('[href="/user/admin/my-views/view/all/builds"]').should('have.text','Build History');
+    })
 });
