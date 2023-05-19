6e5e5898c4607f52cdad916ffcd1643448429c79
TatianaBM <130609785+TatianaBM@users.noreply.github.com>
2023-05-19 14:57:59 +0200

    AT_14.06._002 | Rename Multi-configuration project (#362)
    * AT_14.06._002 | Rename Multi-configuration project

--- cypress/e2e/multiConfigurationProjectRename.cy.js
+++ cypress/e2e/multiConfigurationProjectRename.cy.js
@@ -2,13 +2,16 @@ import projects from '../fixtures/projects.json'
 import messages from '../fixtures/messages.json'
 
 describe('multiConfigurationProjectRename', () => {
-    it('AT_14.06_001 | Rename Multi-configuration project_User enters invalid symbols', () => {
+
+    beforeEach(() => {
         cy.get('a[href="newJob"]').click()
         cy.get('input#name').type(projects.multiConfigurationProject.name)
         cy.get('#j-add-item-type-standalone-projects ul>li:last-child').click()
         cy.get('#ok-button').click()
         cy.get('.jenkins-breadcrumbs__list-item').contains('Dashboard').click()
-        
+    })
+
+    it('AT_14.06_001 | Rename Multi-configuration project_User enters invalid symbols', () => {     
         projects.invalidSymbols.forEach(el => {
         cy.get('td:nth-child(3) a[href^="job/"]').realHover()
         cy.get('.jenkins-table__link button.jenkins-menu-dropdown-chevron').click()
@@ -20,5 +23,15 @@ describe('multiConfigurationProjectRename', () => {
         cy.get('#main-panel p').should('contain', `${el}`).and('contain', messages.renameErrorMessage.specialCharactersMsg)
         cy.get('.jenkins-breadcrumbs__list-item').contains('Dashboard').click()
         })
+    })
+
+    it('AT_14.06._002 | Rename Multi-configuration project_Positive', () => {
+        cy.get('td:nth-child(3) a[href^="job/"]').realHover()
+        cy.get('.jenkins-table__link button.jenkins-menu-dropdown-chevron').click()
+        cy.get('.first-of-type > li').contains('Rename').click()
+        cy.get('input[name="newName"]')
+            .clear()
+            .type(`${projects.multiConfigurationProject.renameWithValidName}{enter}`)
+        cy.get('h1.page-headline').should('include.text', `Project ${projects.multiConfigurationProject.renameWithValidName}`)
     })
 })
