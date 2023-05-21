a1aaff75156ed9e7339518cb6f4f28a72ca4656e
asyniagina <56331881+asyniagina@users.noreply.github.com>
2023-05-20 19:51:01 -0700

    [RF] orgFolderConfigurate - improve locators, added fixtures (#449)

--- cypress/e2e/orgFolderConfigurate.cy.js
+++ cypress/e2e/orgFolderConfigurate.cy.js
@@ -1,8 +1,9 @@
 // <reference types=“cypress”/>
+import orgFolderConfigure from '../fixtures/orgFolderConfigure.json'
 
 function createOrgFolder(orgFolderName) {
     cy.get('a[href="/view/all/newJob"]').click();
-    cy.get('#name').type(orgFolderName);
+    cy.get('#name').type(orgFolderConfigure.orgFolderName);
     cy.get('.jenkins_branch_OrganizationFolder').click();
     cy.get('#ok-button').click();
     cy.get('button[name="Submit"]').click();
@@ -10,25 +11,22 @@ function createOrgFolder(orgFolderName) {
 }
 
 describe('orgFolderConfigurate', () => {
-    const orgFolderName = 'OrgFolderTest';
-    const description = 'The description was added to the organization folder.';
-    const disableMessage = 'This Organization Folder is currently disabled';
 
     it('AT_17.01.002 | Add description to the Organization Folder via Configure', () => {
-        createOrgFolder(orgFolderName);
+        createOrgFolder(orgFolderConfigure.orgFolderName);
         cy.get('.jenkins-table__link.model-link.inside').click();
         cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
-        cy.get('#main-panel > form > div.config-table > div.jenkins-form-item.tr > div.setting-main > textarea').type(description);
-        cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary').click();
-        cy.get('#view-message').should('have.text', description);
+        cy.get('textarea[name="_.description"]').type(orgFolderConfigure.description);
+        cy.get('#bottom-sticker button[name="Submit"]').click();
+        cy.get('#view-message').should('have.text', orgFolderConfigure.description);
     });
 
     it('AT_17.01.001 | Change status folder to disable', () => {
-        createOrgFolder(orgFolderName, '');
+        createOrgFolder(orgFolderConfigure.orgFolderName, '');
         cy.get('.jenkins-table__link.model-link.inside').click();
         cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
         cy.get('.jenkins-toggle-switch__label').click();
-        cy.get('#bottom-sticker > div > button.jenkins-button.jenkins-button--primary').click();
-        cy.get('#enable-project').should('contain', disableMessage);
+        cy.get('#bottom-sticker button[name="Submit"]').click();
+        cy.get('#enable-project').should('contain', orgFolderConfigure.disableMessage);
     });
 });
