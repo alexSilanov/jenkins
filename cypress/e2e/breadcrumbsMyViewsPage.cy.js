c70eb8ef268841c4e39332d4f59a4364295efc23
Halyna A <125617798+7108581@users.noreply.github.com>
2023-05-21 13:28:19 -0700

    [RF] breadcrumbsMyViewsPage- added fixtures (#466)

--- cypress/e2e/breadcrumbsMyViewsPage.cy.js
+++ cypress/e2e/breadcrumbsMyViewsPage.cy.js
@@ -5,83 +5,85 @@ import userMenuItems from '../fixtures/userIconMenuItems.json'
 
  
 describe("breadcrumbsMyViewsPage", () => {
-    it('04.03_001 Verify that user can open selected Pipeline', () => {
+    it('AT_04.03_001|<Breadcrumbs>My Views page > Verify that user can open selected Pipeline', () => {
 
         cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type('testPipeline');
+        cy.get('#name').type(items.pipelineName);
         cy.get('.org_jenkinsci_plugins_workflow_job_WorkflowJob').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
         cy.get('#breadcrumbBar li:first-child').click();
-        cy.get('#page-header .jenkins-menu-dropdown-chevron').click({ force: true })
-        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get('.yuimenuitem').contains(userMenuItems.userMenuItems[2]).click()
         cy.get('a[href="job/testPipeline/"]').click()
         cy.get('#main-panel > h1')
           .should('be.visible')
-          .and('include.text', "testPipeline")
+          .and('include.text', items.pipelineName)
     })
     
-    it('04.03_002 Verify that user can open selected Folder', () => {
+    it('AT_04.03_002| <Breadcrumbs>My Views page > Verify that the user can open the selected Folder', () => {
         cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type('testFolder');
+        cy.get('#name').type(items.folderName);
         cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
-        cy.get('#page-header .jenkins-menu-dropdown-chevron').click({ force: true })
-        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get('.yuimenuitem').contains(userMenuItems.userMenuItems[2]).click()
         cy.get('a[href="job/testFolder/"]').click()
         cy.get('#main-panel > h1')
           .should('be.visible')
-          .and('include.text', "testFolder")
+          .and('include.text', items.folderName)
     })
 
-    it('04.03_003 Verify that user can open selected Freestyle project', () => {
+    it('AT_04.03_003 |<Breadcrumbs>My Views page > Verify that the user can open the selected Freestyle project', () => {
         cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type('testFreestyleProject');
+        cy.get('#name').type(items.freestyleProjectName);
         cy.get('.hudson_model_FreeStyleProject').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
-        cy.get('#page-header .jenkins-menu-dropdown-chevron').click({ force: true })
-        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get('.yuimenuitem').contains(userMenuItems.userMenuItems[2]).click()
         cy.get('#job_testFreestyleProject').click()
         cy.get('a[class="jenkins-table__link model-link inside"]')
           .should('be.visible')
-          .and('include.text', "testFreestyleProject")
+          .and('include.text', items.freestyleProjectName)
     })
-    it('04.03_007 Verify that user can open selected Multi-configuration project', () => {
+
+    it('AT_04.03_007 | Verify that the user can open the selected Multi-configuration project', () => {
         cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type('testMulti Configuration Project');
+        cy.get('#name').type(items.multiConfigurationProjectName);
         cy.get('.hudson_matrix_MatrixProject').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
-        cy.get('#page-header .jenkins-menu-dropdown-chevron').click({ force: true })
-        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get('.yuimenuitem').contains(userMenuItems.userMenuItems[2]).click()
         cy.get('a[class="jenkins-table__link model-link inside"] span')
-          .contains('testMulti Configuration Project')
+          .contains(items.multiConfigurationProjectName)
           .click()
         cy.get('h1[class="matrix-project-headline page-headline"]')
           .should('be.visible')
-          .and('include.text', "testMulti Configuration Project")
+          .and('include.text', items.multiConfigurationProjectName)
     })
-    it('04.03_008 Verify that user can open selected Multibranch Pipeline', () => {
+
+    it('AT_04.03.008 | Verify that the user can open the selected Multibranch Pipeline', () => {
         cy.get('a[href="/view/all/newJob"]').click();
-        cy.get('#name').type('testMultiBranch Pipeline');
+        cy.get('#name').type(items.multiBranchPipelineName);
         cy.get('.hudson_matrix_MatrixProject').click();
         cy.get('#ok-button').click();
         cy.get('.jenkins-button--primary').click();
 
-        cy.get('#page-header .jenkins-menu-dropdown-chevron').click({ force: true })
-        cy.get('.yuimenuitem').contains("My Views").click()
+        cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click()
+        cy.get('.yuimenuitem').contains(userMenuItems.userMenuItems[2]).click()
         cy.get('a[class="jenkins-table__link model-link inside"] span')
-          .contains('testMultiBranch Pipeline')
+          .contains(items.multiBranchPipelineName)
           .click()
         cy.get('h1[class="matrix-project-headline page-headline"]')
           .should('be.visible')
-          .and('include.text', "testMultiBranch Pipeline")
+          .and('include.text', items.multiBranchPipelineName)
     })
 
     it('AT_04.03.009|Verify that the user can open the selected Organization Folder', () => {
