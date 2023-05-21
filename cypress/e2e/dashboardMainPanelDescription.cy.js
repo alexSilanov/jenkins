5bd5be13850c3f3946bb73930fbc37efba53d559
nadilova <nadilova@gmail.com>
2023-05-21 18:20:06 +0200

    AT_02.06.014 | Dashboard > Editing the decription for the project through adding

--- cypress/e2e/dashboardMainPanelDescription.cy.js
+++ cypress/e2e/dashboardMainPanelDescription.cy.js
@@ -8,13 +8,14 @@ describe('dashboardMainPanelDescription', () => {
         cy.get("textarea.jenkins-input").should('not.exist')
     })
 
-    it("AT_02.06_005| Dashboard > Verification of the button 'Add description'", () => {
+    it("AT_02.06_005 | Dashboard > Verification of the button 'Add description'", () => {
         cy.get("a#description-link").click() 
         cy.focused().should('have.attr', 'name', 'description') 
     })
 
-    it("AT_02.06_006| Dashboard > Preview text equals to input description text", () => {
-        cy.get("a#description-link").click() 
+    it("AT_02.06_006 | Dashboard > Preview text equals to input description text", () => {
+        cy.get("a#description-link").click()
+
         cy.get(".jenkins-input").type(descriptionsProject.addDescriptionProject).invoke('val').as('descr')
         cy.get('@descr').then((descr) => {
             cy.get(".textarea-show-preview").click()
@@ -25,6 +26,7 @@ describe('dashboardMainPanelDescription', () => {
     it("AT_02.06_008 | Dashboard > Verification of the link 'Hide preview' for description at main panel", () => {
         cy.get("a#description-link").click() 
         cy.get(".jenkins-input").type(descriptionsProject.addDescriptionProject)
+
         cy.get('.textarea-hide-preview').should('not.be.visible')
         cy.get(".textarea-show-preview").click()
         cy.get('.textarea-hide-preview').should('be.visible')
@@ -33,7 +35,7 @@ describe('dashboardMainPanelDescription', () => {
         cy.get(".textarea-preview").should("not.be.visible")
     })
    
-    it("AT_02.06_010| Dashboard > Add description for the main panel", () => {
+    it("AT_02.06_010 | Dashboard > Add description for the main panel", () => {
         cy.get("a#description-link").click() 
         cy.get(".jenkins-input")
           .type(descriptionsProject.addDescriptionProject)
@@ -41,15 +43,30 @@ describe('dashboardMainPanelDescription', () => {
         cy.get("#description > div:nth-child(1)").should("have.text", descriptionsProject.addDescriptionProject)
     })
 
-    it("AT_02.06_012| Dashboard > Replacing the decription for the project with a new one", () => {
+    it("AT_02.06_012 | Dashboard > Replacing the decription for the project with a new one", () => {
         cy.get("a#description-link").click()
         cy.get(".jenkins-input").type(descriptionsProject.addDescriptionProject) 
         cy.get("button.jenkins-button.jenkins-button--primary").click()
+
         cy.get("a#description-link").click()
         cy.get(".jenkins-input").clear().type(descriptionsProject.addNewDescriptionProject)
         cy.get("button.jenkins-button.jenkins-button--primary").click()
         cy.get("#description > div:nth-child(1)").should("have.text", descriptionsProject.addNewDescriptionProject)
     })
 
+    it("AT_02.06.014 | Dashboard > Editing the decription for the project through adding additional info to the content", () => {
+        cy.get("a#description-link").click() 
+        cy.get(".jenkins-input").type(descriptionsProject.addNewDescriptionProject)
+        cy.get("button.jenkins-button.jenkins-button--primary").click()
+
+        cy.get("#description > div:nth-child(1)").invoke('text').as('descr_current')
+        cy.get('@descr_current').then((descr_current) => {
+            cy.get("a#description-link").click()
+            cy.get(".jenkins-input").type("{end}").type(descriptionsProject.editDescriptionProject) 
+            cy.get("button.jenkins-button.jenkins-button--primary").click()
+            cy.get("#description > div:nth-child(1)").should('have.text', descr_current+descriptionsProject.editDescriptionProject)
+        })
+    })
+    
 })
 
