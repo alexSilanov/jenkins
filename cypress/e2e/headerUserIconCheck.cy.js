e55535c6c8bee41f89a4269f628140c5984a06b0
IraKalinka <90063648+IraKalinka@users.noreply.github.com>
2023-05-21 03:02:20 -0700

    AT_01.03.023 | Header>Verify User Icon has dropdown menu with given links (#456)

--- cypress/e2e/headerUserIconCheck.cy.js
+++ cypress/e2e/headerUserIconCheck.cy.js
@@ -1,5 +1,9 @@
 ///  <reference types="cypress"/>
-describe('AT_01.03.022| Header User icon check', () => {
+
+import {userMenuItems} from '../fixtures/userIconMenuItems.json'
+
+describe('Header User icon check', () => {
+
     it('AT_01.03.022| <Header>User icon check', () =>{
         cy.get('a[href="/user/admin"]').should('be.visible')
         cy.get('.page-header a .jenkins-menu-dropdown-chevron').realHover().click()
@@ -8,4 +12,11 @@ describe('AT_01.03.022| Header User icon check', () => {
         cy.url().should('eq', `http://localhost:${Cypress.env('local.port')}/user/admin/configure`)
        
     })
+
+    it('AT_01.03.023| Header > Verify User Icon has dropdown menu with given links', () => {
+        cy.get('.login button').realHover().click();
+        cy.get('.bd li').should('have.length', userMenuItems.length)
+          .then($els => Cypress._.map($els, 'innerText'))      
+          .should('deep.equal', userMenuItems);       
+    });
 })
