7bf53ab0ce9e987e92bc64ee0f2f6d3f6bd0a768
ViktoriyaED <115054456+ViktoriyaED@users.noreply.github.com>
2023-05-22 13:51:41 -0400

    [RF] AT_09.01_004 | <My Views>Create new view with Job Filters (#461)
    * [RF] AT_09.01_004 | <My Views>Create new view with Job Filters

--- cypress/e2e/myViewCreateNewBasedOnExisting.cy.js
+++ cypress/e2e/myViewCreateNewBasedOnExisting.cy.js
@@ -8,35 +8,32 @@ describe('myViewCreateNewBasedOnExisting', () => {
     function createFolder(name) {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('#name').type(name);
-        cy.get('span.label').contains('Folder').click();
+        cy.get('span.label').contains(myView.folder).click();
         cy.get('#ok-button').click()
         cy.get('button[name=Submit]').click();
-    }    
+    }
+    
+    function createFreestyleJobsInTHeFolder(jobName, folderName) {
+        cy.get(`[href="/job/${folderName}/newJob"]`).click();
+        cy.get('#name').type(jobName);
+        cy.get('span.label').contains(myView.freestyleProject).click();
+        cy.get('#ok-button').click();
+        cy.get('button[name=Submit]').click();
+        cy.get('a[href*="/job/"]').contains(folderName).click();    
+    }
 
     beforeEach('Create new View with two folders and 3 freestyle jobs in one of the folders', () => {        
-        createFolder(myView.FolderName1);    
-        let x = 1;
-        while (x <= 3) {
-            cy.get(`[href="/job/${myView.FolderName1}/newJob"]`).click();
-            cy.get('#name').type('job' + x);
-            cy.get('span.label').contains('Freestyle project').click();
-            cy.get('#ok-button').should('have.text', 'OK').click();
-            cy.get('button[name=Submit]').click();
-            cy.get('.jenkins-breadcrumbs__list-item').find(`[href="/job/${myView.FolderName1}/"]`).click();
-            cy.get('#main-panel h1').then(($el) => {
-                const header = $el.text().trim();
-                expect(header).to.equal(myView.FolderName1)
-            })
-            x++;
-        }
-        cy.get('.jenkins-breadcrumbs__list-item').find('[href="/"]').click();
-
+        createFolder(myView.FolderName1);   
+        createFreestyleJobsInTHeFolder(myView.JobName1, myView.FolderName1); 
+        createFreestyleJobsInTHeFolder(myView.JobName2, myView.FolderName1); 
+        createFreestyleJobsInTHeFolder(myView.JobName3, myView.FolderName1);        
+        cy.get('a[href="/"]').contains(myView.dashboard).click();
         createFolder(myView.FolderName2);
-        cy.get('.jenkins-breadcrumbs__list-item').find('[href="/"]').click();
+        cy.get('a[href="/"]').contains(myView.dashboard).click();
     })
 
     it('AT_09.01_004 | <My Views>Create new view with Job Filters to add jobs from an existing View', () => {
-        cy.get('[href="/newView"]').should('have.attr', 'title', 'New View').click();
+        cy.get('[href="/newView"]').click();
         cy.get('#name').type(myView.ViewName1);
         cy.get('[for="hudson.model.ListView"]').click();
         cy.get('#ok').click();
@@ -50,9 +47,7 @@ describe('myViewCreateNewBasedOnExisting', () => {
         cy.get(`span.jenkins-checkbox label[title="${myView.FolderName2}"]`).click();
         cy.get('#bottom-sticker button[name="Submit"]').click();
         cy.url().should('equal', `http://localhost:${PORT}/view/${myView.ViewName1}/`);
-
-        cy.get('#projectstatus-tabBar [href="/view/view1/"]').should('have.text', `${myView.ViewName1}`);
-        
+       
         cy.get('#projectstatus tbody>tr>td:nth-child(3) a').should('have.length', 3)
         .each(($el, idx) => {
             const expectedUrl = myView.jobFilter[idx].url;
@@ -61,7 +56,7 @@ describe('myViewCreateNewBasedOnExisting', () => {
         cy.get('#projectstatus tbody>tr>td:nth-child(3) span')
         .each(($el, idx) => {
             const expectedName = myView.jobFilter[idx].name;
-            expect($el).to.have.text(expectedName);      
+            expect($el).to.have.text(expectedName);  
         })         
     })
 })
