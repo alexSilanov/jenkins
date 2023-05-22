c54f7cb30706562b37eb73768747e26f59019239
Aleksandra Folkes <124165961+SashaFoxx@users.noreply.github.com>
2023-05-22 09:25:31 -0400

    AT_09.07.003 (#482)

--- cypress/e2e/myViewsEditView.cy.js
+++ cypress/e2e/myViewsEditView.cy.js
@@ -17,4 +17,22 @@ describe('My Views Edit View', () => {
         cy.get('button[name="Submit"]').click();
         cy.get('.task-link-text').should('include.text', data.editView);
     });
+
+    it('AT_09.07.003 | Verify user can click "Edit View" button', () => {
+        cy.get('a[href$="/newJob"]').click();
+        cy.get('input.jenkins-input').type(data.newItem);
+        cy.get('.hudson_model_FreeStyleProject').click();
+        cy.get('#ok-button').click();
+        cy.get('button[name="Submit"]').click();
+        
+        cy.get('a[href="/"].model-link').click();
+        cy.get('a[href="/newView"]').click();
+        cy.get('#name').type(data.viewName);
+        cy.get('label[for="hudson.model.MyView"]').click();
+        cy.get('#ok').click();
+
+        cy.get('a[href$="/configure"]').click();
+        cy.get('[name="name"].jenkins-input').should('have.attr', 'value', data.viewName);
+
+    })
 })
