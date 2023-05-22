1e19c990372f5457a04497a5ceea89e713c85109
Alina-Fedarenka <130203766+Alina-Fedarenka@users.noreply.github.com>
2023-05-22 00:45:35 -0400

    AT_13.05_001|Pipeline|Edit existing description (#471)

--- cypress/e2e/pipelineEditDescription.cy.js
+++ cypress/e2e/pipelineEditDescription.cy.js
@@ -1,22 +1,40 @@
 /// <reference types='cypress'/>
 import pipelineName from '../fixtures/pipelineName.json'
 
-describe('Add description to the pipeline', () =>{
+describe('Add description to the pipeline', () => {
 
-    beforeEach ('Create pipeline', function () {
+    beforeEach('Create pipeline', function () {
         cy.get('a[href="newJob"]').click()
         cy.get('input#name').type('TestPipeline')
         cy.get('li[tabindex="0"] span').contains('Pipeline').click()
         cy.get('#ok-button').click()
         cy.get(':nth-child(1) > .model-link').click()
-        
+
     });
 
     it('Verify User is able to go to tab “Edit description”', () => {
         cy.get('#description-link').click()
         cy.get('.jenkins-input   ').type(pipelineName.descriptionPipeLine)
         cy.get('.jenkins-button--primary ').click();
-        cy.get('#description-link').should('contain','Edit description').click()
+        cy.get('#description-link').should('contain', 'Edit description').click()
         cy.get('textarea[name="description"]').should('have.value', pipelineName.descriptionPipeLine);
     });
+
+    it('AT_13.05_001 | Pipeline | Edit existing description of the pipeline by adding new text to the end', () => {
+        cy.get('a[href="job/TestPipeline/"] span').click()
+        cy.get('#description-link').click()
+        cy.get('textarea.jenkins-input')
+          .should('have.text', '')
+          .type(pipelineName.descriptionPipeLine)
+        cy.get('button[name="Submit"]').first().click()
+        cy.get(':nth-child(1) > .model-link').click()
+        cy.get(':nth-child(5) .task-link-wrapper .task-link').click()
+        cy.get('a[href="job/TestPipeline/"]').click()
+        cy.get('#description-link').click()
+        cy.get('.jenkins-input   ').type('{moveToEnd}').type(pipelineName.additionalDescriptionPipeline)
+        cy.get('button[name="Submit"]').first().click()
+        cy.get('#description div:nth-child(1)')
+          .should('have.text', pipelineName.descriptionPipeLine + pipelineName.additionalDescriptionPipeline)
+    });
 })
+
