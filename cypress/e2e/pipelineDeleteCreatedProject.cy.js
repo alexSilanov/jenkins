7291aa519699f8acfaf53a0cbc9aafc6182149c9
Daria <124002954+DariaMas7@users.noreply.github.com>
2023-05-22 13:07:55 -0700

    [RF] pipelineDeleteCreatedProject|AT_13.02_001/002 (#504)

--- cypress/e2e/pipelineDeleteCreatedProject.cy.js
+++ cypress/e2e/pipelineDeleteCreatedProject.cy.js
@@ -2,21 +2,20 @@
 import pipelineName from '../fixtures/pipelineName.json'
 
 describe('Pipeline | Delete Created Project', () => {
-    function createPipeline() {
-        cy.get('span[class="task-link-text"]').contains('New Item').click({ force: true })
+    beforeEach('Create New Pipeline',()=>{
+        cy.get('span[class="task-link-text"]').contains(pipelineName.textForNewItem).click({ force: true })
         cy.get('[name="name"]').type(pipelineName.newPipelineName)
-        cy.get('.label').contains('Pipeline').click()
+        cy.get('.label').contains(pipelineName.pipelineJob).click()
         cy.get('#ok-button').click()
         cy.get("button[name='Submit']").click()
         cy.get('.jenkins-breadcrumbs__list-item').click()
         cy.get('[class="jenkins-table__link model-link inside"] span').contains(pipelineName.newPipelineName).should('exist')
-    }
+    })   
+    
     it('AT_13.02_001 | Pipeline Delete created project using dropdown menu', () => {
-        createPipeline()
-
         cy.get('tbody tr td a.jenkins-table__link').realHover({ position: "center" })
         cy.get('#projectstatus .jenkins-menu-dropdown-chevron').should('be.visible').click()
-        cy.get('.first-of-type :nth-child(4) .yuimenuitemlabel').should('contain', 'Delete Pipeline').click()
+        cy.get('.first-of-type :nth-child(4) .yuimenuitemlabel').should('contain', pipelineName.textForDeleteProject).click()
 
         cy.on('window:confirm', (str) => {
             expect(str).to.equal(pipelineName.confirmationOfDeleting)
@@ -25,13 +24,11 @@ describe('Pipeline | Delete Created Project', () => {
     })
     
     it('AT_13.02_002 | Pipeline |Delete created project Pipeline in left side bar',()=>{
-        createPipeline()
-
         cy.get('[class="jenkins-table__link model-link inside"] span')
           .should('have.text',pipelineName.newPipelineName)
           .click()
         cy.get(':nth-child(6) .task-link-wrapper .task-link span')
-          .contains('Delete Pipeline')
+          .contains(pipelineName.textForDeleteProject)
           .click()
 
         cy.on('window:confirm', (str) => {
