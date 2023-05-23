9d0c74309856c8186116f5c264702a9ba23bcba1
Olga-QA-USA <115054949+Olga-QA-USA@users.noreply.github.com>
2023-05-23 15:47:59 -0400

    AT_01.02_009 verify Search Box (#498)

--- cypress/e2e/pageHeaderSearchBox.cy.js
+++ cypress/e2e/pageHeaderSearchBox.cy.js
@@ -1,9 +1,13 @@
 /// <reference types="cypress"/>
 
-describe('Jenkins Search Box', () => {
+describe('pageHeaderSearchBox', () => {
     it('AT_01.02_005 verify Search Box is visible on the home page', function () {
         cy.get('#searchform').should('be.visible').click().type('project')
         cy.get('#search-box-sizer').should('have.text', 'project')
     });
     
+    it('AT_01.02_009 verify Search Box is visible on the New Item page', function () {
+        cy.get('.task  a[href="/view/all/newJob"]').click()
+        cy.get('#searchform').should('be.visible').click().type('project') 
+    });
 });
