44dad26763af1d540466145d0f75a26da8a82e08
Paul Korz <131908496+Paul-Korz@users.noreply.github.com>
2023-05-23 22:39:22 +0300

    AT_01.08_015 | Verify Successful Logout Functionality (#540)

--- cypress/e2e/headerLogOutButton.cy.js
+++ cypress/e2e/headerLogOutButton.cy.js
@@ -6,4 +6,10 @@ describe('Header | Log Out Button', () => {
         cy.get('#page-header > div.login.page-header__hyperlinks > a:nth-child(4)').contains('log out').click()
         cy.url().should('include',`http://localhost:${Cypress.env('local.port')}/login?from=%2F`)
     })
+
+    it('AT_01.08_015 | Verify Successful Logout Functionality', () => {
+        cy.get('a[href="/logout"]').click()
+        cy.get('input[placeholder="Username"]').should('be.visible')
+        cy.get('input[placeholder="Password"]').should('be.visible')
+    })
 })
