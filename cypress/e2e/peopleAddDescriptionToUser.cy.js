6525f9bfcdbee732b2f4902a561b1662b01fde4b
MarinaMikhailova <120079100+MarinaMikhailova@users.noreply.github.com>
2023-05-23 16:55:05 -0700

    refactor_AT_06.02_003 | Verify save button (#555)
    Co-authored-by: Geronimo <geronimo@iMac-Geronimo.local>

--- cypress/e2e/peopleAddDescriptionToUser.cy.js
+++ cypress/e2e/peopleAddDescriptionToUser.cy.js
@@ -4,8 +4,7 @@ import userDescription from "../fixtures/userDescription.json";
 const USERNAME = Cypress.env('local.admin.username');
 
 describe("peopleAddDescriptionToUser", () => {
-  const newDescription = 'new user description';
-
+  
   it("AT 06.02.001 | Verify description is added to user", function () {
     cy.get("a.task-link").eq(1).click();
     cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click();
@@ -15,15 +14,15 @@ describe("peopleAddDescriptionToUser", () => {
     cy.get('#description div:nth-of-type(1)').should("have.text", userDescription.textDescription);
   });
 
-  it.skip('AT_06.02_003 | Verify save button functionality', () => {
+  it('AT_06.02_003 | Verify save button functionality', () => {
     cy.get(':nth-child(2) > .task-link-wrapper > .task-link').click();
-    cy.get('.jenkins-table__link').click();
+    cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click();
     cy.get('#description-link').click();
-    cy.get('.jenkins-input').type(newDescription);
+    cy.get('.jenkins-input').clear().type(userDescription.newDescription);
     cy.get('.jenkins-button').click();
     cy.get(':nth-child(1) > .task-link-wrapper > .task-link').click();
-    cy.get('.jenkins-table__link').click();
-    cy.get('#description').should('include.text','new user description')
+    cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click();
+    cy.get('#description').should('include.text',userDescription.newDescription);
     
   });
   it.skip("AT_06.02_002 | Verify description is added to a user", function () {
