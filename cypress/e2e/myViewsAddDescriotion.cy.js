ac6751d9e354fec45c9ab165b5948dca2c41386b
vlada4nik <vlada.nikit4enko@gmail.com>
2023-05-23 12:35:03 -0700

    AT_09.02.001|My Views Add Discript

--- cypress/e2e/myViewsAddDescriotion.cy.js
+++ cypress/e2e/myViewsAddDescriotion.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import myViewsAddDescr from "../fixtures/myViewsAddDescr.json"
 describe('<My Views > Add description',() => {
     it ('<My Views Add description>',function(){
         cy.contains('My Views').click() 
@@ -6,5 +7,17 @@ describe('<My Views > Add description',() => {
         cy.get('.jenkins-input').type('description')
         cy.get('.jenkins-button--primary').click()
     })
+    
+    it('Add descriprions of my views',()=>{
+        cy.get('a[href*="/me/my-views"]').click()
+        cy.get('#description-link').should('be.visible').click()
+        cy.get('.jenkins-input').click().type(myViewsAddDescr.describeText)
+        cy.get('button[name="Submit"]').click()
+        cy.get('#description').should('be.visible').contains(myViewsAddDescr.describeText)
+        cy.get('a[href="editDescription"]').should('be.visible')
+    })
 })
+
+
+//})
 
