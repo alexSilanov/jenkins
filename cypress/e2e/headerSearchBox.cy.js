bfff3c54c7d869b16ac29ecf3b5d2d04570d17cc
IharStankevich <133066957+IharStankevich@users.noreply.github.com>
2023-05-23 08:11:34 -0400

    AT_01.02.026 | Search box is not available (#514)

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -101,9 +101,13 @@ describe('Header Search Box', () => {
     cy.get('#breadcrumb-menu a[href="/me/my-views"]').click()
     cy.get('#searchform').should('be.visible')
   });
+  
+  it('AT_01.02.026 | Search box is not available for not logged in users ', () =>{
+    cy.get('a[href="/logout"]').click();
+    cy.get('#searchform').should('not.exist'); 
+  });
 
   it('AT_01.02_023 | Validation of the Search box', ()=> { 
     cy.get('#search-box').should('have.attr','placeholder','Search (CTRL+K)') 
   });
-
 });
