580a603a1eb86a14b169a12f2ef52b122cfc1bea
Elena Kanash <78100493+ElenaKanash@users.noreply.github.com>
2023-05-25 16:43:23 +0300

    AT_01.01_43 | Header| Head Icon is visible and redirects to the homepage (#600)

--- cypress/e2e/headerHeadIcon.cy.js
+++ cypress/e2e/headerHeadIcon.cy.js
@@ -20,7 +20,7 @@ describe('Header Head Icon', () => {
         cy.get('#jenkins-home-link').should('be.visible').click()
         cy.url().should('eq','http://localhost:8080/')
     })
-  
+
     it('AT_01.01_033 | Validate <Header> head icon', () => {
         cy.get('span.task-link-text').contains('People').click({ force: true });
         cy.get('div h1').should('exist')
@@ -37,7 +37,7 @@ describe('Header Head Icon', () => {
             .click()
         cy.get('h1').should('have.text', 'Welcome to Jenkins!')
     })
-  
+
     it('AT_01.01_033 | Validate <Header> head icon', () => {
         cy.get('span.task-link-text').contains('People').click({force: true});
         cy.get('div h1').should('exist')
@@ -47,7 +47,7 @@ describe('Header Head Icon', () => {
                         .and('be.visible');
 
     })
-    
+
     it('AT_01.01_038 | Head Icon is visible and redirects to home page after clicking', () => {
         cy.get('a[href="newJob"]').click();
         cy.get('#jenkins-head-icon').should('be.visible').click();
@@ -60,9 +60,9 @@ describe('Header Head Icon', () => {
           .get('header')
           .should('be.visible');
         cy.get('#jenkins-head-icon').click();
-        cy.get('div h1').should('have.text','Welcome to Jenkins!').and('be.visible');  
+        cy.get('div h1').should('have.text','Welcome to Jenkins!').and('be.visible');
     })
-  
+
     it('AT_01.01_39 | Head icon is visible, clickable and redirects to the home page', () => {
         cy.get('span.task-link-text').contains('People').click({force:true});
         cy.get('#jenkins-name-icon').click();
@@ -74,7 +74,7 @@ describe('Header Head Icon', () => {
         cy.get('#main-panel h1').should('include.text', 'People')
         cy.get('.logo #jenkins-head-icon').should('be.visible').click()
         cy.url().should('include', `http://localhost:${Cypress.env('local.port')}/`)
-        cy.get('#main-panel h1').should('have.text', logInPage.loginPageHeader) 
+        cy.get('#main-panel h1').should('have.text', logInPage.loginPageHeader)
     })
 
     it('AT_01.01_40 | Head Icon is visible in top left corner', () => {
@@ -90,6 +90,14 @@ describe('Header Head Icon', () => {
         cy.get('.empty-state-block > h1').should('contain', logInPage.loginPageHeader)
     })
 
+    it('AT_01.01_43 | Header| Head Icon is visible and redirects to the homepage', () => {
+        const LOCAL_PORT = Cypress.env('local.port')
+
+        cy.get('#side-panel a[href="/view/all/newJob"]').click()
+        cy.get('header #jenkins-head-icon').should('be.visible').click()
+
+        cy.url().should('equal', `http://localhost:${LOCAL_PORT}/`)
+    })
 
 })
 
