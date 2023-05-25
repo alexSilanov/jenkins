92caea6f76ad112f6145d7c337807c06a4f423d9
KatyaKaterina33 <115548501+KatyaKaterina33@users.noreply.github.com>
2023-05-25 09:29:37 +0200

    RF AT_09.02_005| My Views>Add description (#548)

--- cypress/e2e/myViewsAddDescriptions.cy.js
+++ cypress/e2e/myViewsAddDescriptions.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress"/>
+import descriptionsProject from '../fixtures/descriptionsProject.json'
+import createNewView from '../fixtures/createNewView.json'
 
 describe('My Views add description', () => {
 
@@ -10,13 +12,16 @@ describe('My Views add description', () => {
         cy.get('#description div:nth-child(1)').should('have.text','DESCRIPTION')
     })
 
-    let inputText = 'description';
     it('AT 09.02.005| My Views> Add description', function () {
-        cy.contains('My Views').click();
+        cy.contains(createNewView.myView).click();
         cy.get('#description-link').click();
-        cy.get('.jenkins-input').should('be.visible').clear().type(inputText);
+        cy.get('.jenkins-input')
+            .should('be.visible')
+            .clear()
+            .type(descriptionsProject.addDescriptionProject);
         cy.get('button[name="Submit"]').click();
-        cy.get('#description>div:nth-child(1)').should('have.text', inputText);
+        cy.get('#description>div:nth-child(1)')
+            .should('have.text', descriptionsProject.addDescriptionProject);
     });
 
 })
