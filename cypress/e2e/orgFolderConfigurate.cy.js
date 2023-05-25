c63b1fe81d7cbb4e1eadae390da2d388f300b2cb
Ekaterina Blinova <132958390+EkBlinova@users.noreply.github.com>
2023-05-25 15:39:44 -0700

    AT_17.01_005 | Verify that the user can add a display name for the organization folder (#607)
    * AT_17.01_005 | User can add a display name for the organization folder

--- cypress/e2e/orgFolderConfigurate.cy.js
+++ cypress/e2e/orgFolderConfigurate.cy.js
@@ -1,5 +1,7 @@
 // <reference types=“cypress”/>
 import orgFolderConfigure from '../fixtures/orgFolderConfigure.json'
+import headers from '../fixtures/headers.json'
+import pages from '../fixtures/pages.json'
 
 function createOrgFolder(orgFolderName) {
     cy.get('a[href="/view/all/newJob"]').click();
@@ -39,5 +41,16 @@ describe('orgFolderConfigurate', () => {
         cy.get('#bottom-sticker button[name="Submit"]').click();
         cy.get('a[href="./configure"]').click()
         cy.contains(orgFolderConfigure.displayName).should('be.visible')
+    })
+
+    it('AT_17.01_005 | Verify that the user can add a display name for the organization folder', function () {
+
+        createOrgFolder(orgFolderConfigure.orgFolderName)
+        cy.get('#main-panel .jenkins-table__link.model-link.inside').click()
+        cy.get('#tasks').contains(pages.configurationPage).click()
+        cy.get('#general > span').should('have.text', headers.generalHeader)
+        cy.get('input[name="_.displayNameOrNull"]').click().type(orgFolderConfigure.displayName)
+        cy.get('button[name="Submit"]').click()
+        cy.get('#main-panel h1').should('contain', orgFolderConfigure.displayName)
     })
 });
