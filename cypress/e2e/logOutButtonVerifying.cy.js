cda3a2b9b0a3bcd3adbe2d62c548f3718c59155e
Helen.Nosenko <80576283+lina2989@users.noreply.github.com>
2023-05-26 21:43:07 +0300

    AT_01.08_028 | Header Log out button (#531)
    * AT_01.08_028
    
    * updated
    
    * fix
    
    * added new title verification
    
    * FF
    
    * RF2

--- cypress/e2e/logOutButtonVerifying.cy.js
+++ cypress/e2e/logOutButtonVerifying.cy.js
@@ -1,8 +1,10 @@
 /// <reference types="cypress"/>
+import loginPage from '../fixtures/logInPage.json';
 
 describe('Header Log out button', () => {
-  it('Verify logout button is clickable and redirects to login page', function () {
-    cy.get('a[href="/logout"]').should('be.visible').and('have.text','log out').click();
-    cy.url().should('contain', 'login');    
-  });      
+   
+  it('AT_01.08_028', function (){
+    cy.get('body > header:nth-child(2) > div:nth-child(3) > a:nth-child(4) > span:nth-child(2)').click()
+   })
+     
 });
