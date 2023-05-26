1d7d15f9ef6131a7c4092b1b31167e090477d5b3
Halyna A <125617798+7108581@users.noreply.github.com>
2023-05-25 21:02:59 -0700

    [RF] Changed AT_04.03.012 | <Breadcrumbs>My Views (#622)

--- cypress/e2e/breadcrumbsMyViewsPage.cy.js
+++ cypress/e2e/breadcrumbsMyViewsPage.cy.js
@@ -162,22 +162,18 @@ describe("breadcrumbsMyViewsPage", () => {
     cy.get('.hudson_model_FreeStyleProject').click();
     cy.get('#ok-button').click();
     cy.get('.jenkins-button--primary').click();
-    cy.get('#jenkins-home-link').click();
-
-    cy.get('#page-header .jenkins-menu-dropdown-chevron').realHover().click();
-    cy.get('.yuimenuitem').contains(userMenuItems.userMenuItems[2]).click();
-    cy.get('a[class="jenkins-table__link model-link inside"]')
-      .should('include.text', items.createdBuildsNames[0]);
+    cy.get(':nth-child(1) > .model-link').click();
     cy.get('.build-status-icon__outer').trigger('focus')
     cy.get('svg[tooltip="Not built"]').should('be.visible');
     cy.get('td:nth-child(4)').should('contain', items.cellData);
     cy.get('td:nth-child(5)').should('contain', items.cellData);
     cy.get('td:nth-child(6)').should('contain', items.cellData);
     cy.get('td:nth-child(7)').trigger('focus');
+
     cy.get('a[tooltip="Schedule a Build for Build Freestyle"]')
       .should('be.visible').click()
-    cy.wait(600)
-    cy.get('#jenkins-home-link').click();
+    cy.wait(2000)
+    cy.get(':nth-child(1) > .model-link').click();
     cy.get('.build-status-icon__outer').trigger('focus');
     cy.get('svg[tooltip="Success"]').should('be.visible');
   });
