2d76307d2015863dcf40c83b0d537df726484351
Olha Alekseeva <108941080+Olgalex@users.noreply.github.com>
2023-05-28 21:14:24 +0300

    AT_05.04.03 | <New item> User is able to create Folder project (#643)
    * AT_05.04.03
    
    * RF
    
    * RF2

--- cypress/e2e/createJob.cy.js
+++ cypress/e2e/createJob.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress"/>
 
+import {header, newPageUrl, text, folder, folderName} from '../fixtures/createFolderProject.json'
+
 describe('AT_02.01_002 | Homepage Create a clickable job link', () => {
     it('Create a clickable job link', function () {
         cy.get('li .content-block__link').contains('Create a job').should('be.visible')
@@ -9,4 +11,14 @@ describe('AT_02.01_002 | Homepage Create a clickable job link', () => {
         cy.get('#ok-button').click()
         cy.get('#general').should('be.visible')
     });
+    it ('AT_05.04.03 | <New item> User is able to create Folder project', () => {
+        cy.get("a[href='/view/all/newJob']").should('have.text', header).click()
+        cy.url().should("eq", newPageUrl)
+        cy.get('#name').type(text).should('be.visible')
+        cy.get('.label').contains(folder).click();
+        cy.get('#ok-button').click()
+        cy.get('#general').should('be.visible')
+        cy.get("button[name='Submit']").click()
+        cy.get("div[id='main-panel'] h1").should('have.text', folderName)          
+    })
 });
