6f95736e2bd8cc0679b9cbe01ec0493715c22bd0
S-Lanchik <130118964+S-Lanchik@users.noreply.github.com>
2023-05-28 21:47:27 -0700

    AT_01.07.006 | Verify Redirection to Credential (#644)

--- cypress/e2e/headerCredentialsMenuLink.cy.js
+++ cypress/e2e/headerCredentialsMenuLink.cy.js
@@ -42,6 +42,13 @@ describe('headerCredentialsMenuLink', () => {
     cy.get('#main-panel h1').should('have.text', headerCredentials.newDomainPageHeader)
     })
 
+  it('AT_01.07.006 | Verify Redirection to Credential Page from User Dropdown Menu', () => {
+    cy.get('header .jenkins-menu-dropdown-chevron').click()
+    cy.get('#breadcrumb-menu li:last-child').click()
+
+    cy.get('#main-panel h1').should('have.text', headerCredentials.credentialsPageHeader)
+  });
+
     it('AT_01.07.007 | <Header> Credentials menu link/ access check', function() {
       cy.get('.page-header__hyperlinks .model-link').click()
       cy.get('a[href*="/credentials').click()
