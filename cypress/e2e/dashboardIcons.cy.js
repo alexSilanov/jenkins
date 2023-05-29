f48e42043b21fb460a6ad0db73832d4e8eb46521
Maksim Ozhegov <121754923+ozhegov@users.noreply.github.com>
2023-05-29 22:12:50 +0300

    [RF] dashboardIcons | AT_20.01_003 | Dashboard > Verify table size is L (#658)

--- cypress/e2e/dashboardIcons.cy.js
+++ cypress/e2e/dashboardIcons.cy.js
@@ -5,16 +5,16 @@ import iconsSML from "../fixtures/iconsSML.json"
 
 describe('dashboardIcons', () => {
 
-    it('AT_20.01_003 | Dashboard > Verify table size is L', () => {
-
+    beforeEach('Create a freestyle project', () => {
         cy.get('a[href="newJob"]').click()
         cy.get('input#name').type(projects.newProject)
         cy.get('.hudson_model_FreeStyleProject').click()
         cy.get('button#ok-button').click()
         cy.get('button.jenkins-button--primary ').click()
-
         cy.get('#jenkins-home-link').click()
+    });
 
+    it('AT_20.01_003 | Dashboard > Verify table size is L', () => {
         cy.get('[tooltip="Large"]').click()
         cy.get('#projectstatus').then((obj) => {
             cy.document().then(() => {
