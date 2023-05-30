c8beb1045b4c6051a74106d3c3ce202788ebc79e
Olesia Matveeva <118187575+matvlesia@users.noreply.github.com>
2023-05-30 20:31:32 +0100

    AT_07.02.006 | Verify user can sort builds (#680)

--- cypress/e2e/buildHistorySortBuilds.cy.js
+++ cypress/e2e/buildHistorySortBuilds.cy.js
@@ -20,7 +20,7 @@ function createBuildsOfNewProject(projectName, buildsNumber) {
 };
 
 describe('Build History Sort builds', () => {
-
+    
     it('AT_07.02 _001 | Build History Sort builds', () => {
         const sortColumn = () => cy.get('table#projectStatus thead .sortheader');
         const buildColumn = () => sortColumn().contains('Build').realHover();
@@ -116,5 +116,15 @@ describe('Build History Sort builds', () => {
             expect(cellTexts).to.deep.equal(sortedSelectCells);
         })
     });
-});
 
+    it('AT_07.02.006 | Verify user can sort buids', () => {
+        createBuildsOfNewProject(projects.projects[0], 3)
+        cy.get('a[href="/view/all/builds"]').click()
+        cy.get('thead>tr>th:nth-child(2)>a').click().click()
+        cy.get('.jenkins-table__badge').then(($els)=>{
+            let actualResult = Cypress.$.makeArray($els).map(($el)=> $el.innerText);
+            let expectedResult = actualResult.slice().sort()
+            expect(actualResult).to.deep.equal(expectedResult)
+        })
+    })
+});
