6b8d0f897a8f905fb4150dcce01984d963a49784
Kristina Ozhog <kristinka_kuku@mail.ru>
2023-05-30 15:39:03 +0100

    deleted beforeEach

--- cypress/e2e/navigationWithBreadcrumbs.cy.js
+++ cypress/e2e/navigationWithBreadcrumbs.cy.js
@@ -3,11 +3,8 @@
 import descriptionOnUserPage from '../fixtures/descriptionOnUserPage.json'
 
 describe('navigationWithBreadcrumbs', () => {
-    beforeEach(() => {
-        cy.get('#side-panel div#tasks div:nth-child(5) a').click()
-    })
-    
     it('AT_04.01_01 | Verify clickability of user name link in Breadcrumbs', () => {
+        cy.get('#side-panel div#tasks div:nth-child(5) a').click()
         cy.get('#breadcrumbBar li:nth-child(3)>a').click()
         cy.get('#main-panel > div:nth-child(4)').contains(descriptionOnUserPage.JenkinsUserId)
     })
