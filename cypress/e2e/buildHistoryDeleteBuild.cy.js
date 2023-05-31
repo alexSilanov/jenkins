420096b1940de78976331d0936ad157673a7145b
Maksim Ozhegov <121754923+ozhegov@users.noreply.github.com>
2023-05-31 22:40:39 +0300

    [RF] buildHistoryDeleteBuild | AT_07.03.003 | Build History > Verify deleting builds clicking on “Delete build” option in dropdown menu (#715)

--- cypress/e2e/buildHistoryDeleteBuild.cy.js
+++ cypress/e2e/buildHistoryDeleteBuild.cy.js
@@ -35,7 +35,7 @@ describe('buildHistoryDeleteBuild', () => {
         cy.get('svg.icon-nobuilt').should('be.visible')
     });
 
-    it.only('AT_07.03.003 | Build History > Verify deleting builds clicking on “Delete build” option in dropdown menu', () => {
+    it('AT_07.03.003 | Build History > Verify deleting builds clicking on “Delete build” option in dropdown menu', () => {
         cy.get('#tasks div:nth-child(3)').click()
         cy.get('.jenkins-table__link.jenkins-table__badge.model-link.inside').realHover().click()
         cy.get('.icon-edit-delete.icon-md').click()
