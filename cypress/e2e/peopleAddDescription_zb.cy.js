f449bcd941efd9e2db0979a74f19196d9ac633c4
Alla Anderson <76411309+allamalitska@users.noreply.github.com>
2023-05-31 16:22:10 -0700

    AT_06.02_008 | <People> Verify the ability to add a description to a user (#721)

---
+++ cypress/e2e/peopleAddDescription_zb.cy.js
@@ -0,0 +1,16 @@
+import userDescription from '../fixtures/userDescription.json'
+const USERNAME = Cypress.env('local.admin.username');
+
+describe('People Add Discription to User', () => {
+
+    it('AT_06.02_008 | <People> Verify the ability to add a description to a user', () => {
+        cy.get('#tasks .task:nth-child(2)').click()
+        cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click()
+        cy.get('[href="editDescription"]').click()
+        cy.get('textarea[name="description"]').clear().type(userDescription.description)
+        cy.get('[formnovalidate="formNoValidate"]').click()
+
+        cy.get('#description div:nth-child(1)').should('include.text', `${userDescription.description}`)
+    })
+
+})
