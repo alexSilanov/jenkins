a5c4ae00775bc2087d6c3ed2fd10f485349a53cf
Viktoria Voloshina <112795298+VoloshinaV@users.noreply.github.com>
2023-06-01 09:48:30 -0700

    AT_06.04.003 | <People> | Edit Users description (#728)

--- cypress/e2e/peopleAddDescription_zb.cy.js
+++ cypress/e2e/peopleAddDescription_zb.cy.js
@@ -17,12 +17,12 @@ describe('People Add Discription to User', () => {
     })
 
     it('AT_06.02.006 | <People> | Verify adding description to the user',() => {
-        cy.get('a[href="/asynchPeople/"]').click();
+        cy.get('a[href="/asynchPeople/"]').click()
         cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click()
         cy.get('#description-link').click();
         cy.get('textarea[name="description"]').clear().type(userDescription_zb.addNewDescription)
         cy.get('button[name="Submit"]').click()
         cy.get('#description div:nth-of-type(1)').should('have.text', userDescription_zb.addNewDescription)
-      })
+    })
 
 })
--- cypress/e2e/peopleEditDescription_zb.cy.js
+++ cypress/e2e/peopleEditDescription_zb.cy.js
@@ -2,6 +2,7 @@
 
 import userDescription from '../fixtures/userDescription.json'
 import descriptionsProject from '../fixtures/descriptionsProject.json'
+import userDescription_zb from "../fixtures/userDescription_zb.json";
 
 const USERNAME = Cypress.env('local.admin.username');
 
@@ -20,6 +21,20 @@ describe('People Edit Discription to User', () => {
         cy.get('[formnovalidate="formNoValidate"]').click()
         cy.get('#description div:nth-child(1)').should('have.text', userDescription.editDescription)
 
+    })
+
+    it('AT_06.04.003 | <People> | Edit Users description',() => {
+        cy.get('a[href="/asynchPeople/"]').click()
+        cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click()
+        cy.get('#description-link').click()
+        cy.get('textarea[name="description"]').clear().type(userDescription_zb.oldDescription)
+        cy.get('button[name="Submit"]').click()
+        cy.get('#description div:nth-of-type(1)').should('have.text', userDescription_zb.oldDescription)
+        
+        cy.get('#description-link').click();
+        cy.get('textarea[name="description"]').clear().type(userDescription_zb.editOldDescription)
+        cy.get('button[name="Submit"]').click()
+        cy.get('#description div:nth-of-type(1)').should('have.text', userDescription_zb.editOldDescription)
     })
 })
 
