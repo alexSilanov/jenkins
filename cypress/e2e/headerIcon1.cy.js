d347b7ef74446057f79ad671d8155c8071717906
KseniyaParfianovich <117307555+KseniyaParfianovich@users.noreply.github.com>
2023-06-01 14:04:20 +0200

    [RF] headerIcon1 | AT_01.01 _021 (#731)
    * [RF] headerIcon1 | AT_01.01 _021
    
    * remove only
    
    * add fixture and command:navigateJenkinsHeaderIcon
    
    * remove space

--- cypress/e2e/headerIcon1.cy.js
+++ cypress/e2e/headerIcon1.cy.js
@@ -1,11 +1,23 @@
 /// <reference types="cypress" />
+import pages from "../fixtures/pages.json"
 
 describe("Header Icon 1", () => {
-  it("AT_01.01 _021| Verify Head Icon is clickable.", () => {
-    cy.visit("http://localhost:8080/");
-    cy.get("#jenkins-head-icon").should("be.visible");
+
+  Cypress.Commands.add('navigateUserConfigurationPage', () => {
+    cy.get('.login .model-link').should('be.visible');
+    cy.get('#page-header .login a.model-link button.jenkins-menu-dropdown-chevron').realHover().click();
+    cy.get('#breadcrumb-menu li.yuimenuitem a span').contains('Configure').click();
+});
+
+  Cypress.Commands.add('navigateJenkinsHeaderIcon', () => {
+  cy.get("#jenkins-head-icon").should("be.visible");
     cy.get("#jenkins-head-icon").click();
-    cy.url().should("eq", "http://localhost:8080/");
+});
+
+  it("AT_01.01 _021| Verify Head Icon is clickable.", () => {
+    cy.navigateUserConfigurationPage();
+    cy.navigateJenkinsHeaderIcon();
+    cy.title().should("eq", pages.dashboardPageTitle);
   });
 
     it('AT_01.01_044 | <Header> Head Icon is visible and and clickable', ()=>{
