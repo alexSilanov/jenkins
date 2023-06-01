d4f490f84329b0e84dcbc76c61b3319baa6ff022
Viktoria Voloshina <112795298+VoloshinaV@users.noreply.github.com>
2023-05-31 23:12:28 -0700

    AT_06.02.006 | <People> | Verify adding description to the user (#723)
    * AT_06.02.006 | <People> | Verify adding description to the user
    
    * Locator was changed

--- cypress/e2e/peopleAddDescription_zb.cy.js
+++ cypress/e2e/peopleAddDescription_zb.cy.js
@@ -1,4 +1,7 @@
+/// <reference types='cypress'/>
+
 import userDescription from '../fixtures/userDescription.json'
+import userDescription_zb from "../fixtures/userDescription_zb.json";
 const USERNAME = Cypress.env('local.admin.username');
 
 describe('People Add Discription to User', () => {
@@ -12,5 +15,14 @@ describe('People Add Discription to User', () => {
 
         cy.get('#description div:nth-child(1)').should('include.text', `${userDescription.description}`)
     })
+
+    it('AT_06.02.006 | <People> | Verify adding description to the user',() => {
+        cy.get('a[href="/asynchPeople/"]').click();
+        cy.get(`a[href*='/user/${USERNAME.toLowerCase()}']`).click()
+        cy.get('#description-link').click();
+        cy.get('textarea[name="description"]').clear().type(userDescription_zb.addNewDescription)
+        cy.get('button[name="Submit"]').click()
+        cy.get('#description div:nth-of-type(1)').should('have.text', userDescription_zb.addNewDescription)
+      })
 
 })
