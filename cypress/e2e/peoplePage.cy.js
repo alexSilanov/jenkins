6558d0163c86ffd1ad50f6857bf78245d0e82456
Naz <106405355+Nzs1@users.noreply.github.com>
2023-06-01 16:27:45 +0100

    AT_06.01_007 | Verify of url "People" page (#736)
    * AT_06.01_007 | Verify of url "People" page
    
    * changed url

--- cypress/e2e/peoplePage.cy.js
+++ cypress/e2e/peoplePage.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import pagePeopleText from "../fixtures/pagePeopleText.json"
+import homePage from "../fixtures/homePage.json"
 
 describe("People page", () => {
     it("AT_6.01_002 | People page tab", () => {
@@ -11,5 +12,10 @@ describe("People page", () => {
     it('Verify redirection to People page', () => {
         cy.get('a[href="/asynchPeople/"]').click();
         cy.get('.jenkins-app-bar__content').should('have.text', pagePeopleText.textPeoplePage);
+    })
+
+    it ('AT_06.01_007 | Verify of url "People" page', () => {
+        cy.get('#side-panel').contains(homePage.sidePanelItems[1]).click()
+        cy.url().should('include', homePage.endPointUrl[1])
     })
 });
