ffbee7fe747edda9991d34400e764187905fd6d3
Assem Saginayeva <asemsaginayeva@gmail.com>
2023-06-01 13:16:58 +0200

    AT_01.08.031 | Header Log out button (#707)
    AT_01.08.031 | Header Log out button

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -5,7 +5,7 @@ describe('verification log out button', function () {
 
     it('01_08_001 log out button should be clickable and open login page', function () {
         cy.get('a[href="/logout"]').should('be.visible').click()
-        cy.url().should('contain','http://localhost:8080/login')
+        cy.url().should('contain', 'http://localhost:8080/login')
     });
 
     it('Verify that the Log out button transfer the user back to the login page "Welcome to Jenkins!"', () => {
@@ -17,13 +17,23 @@ describe('verification log out button', function () {
         cy.get('[href="/logout"] > .hidden-xs').click();
         cy.get('#loginIntroDefault').should('have.text', 'Welcome to Jenkins!')
     })
-    it('AT_01.08_14|Verify Log out button is working ', () =>{
+    it('AT_01.08_14|Verify Log out button is working ', () => {
         cy.get('a[href="/logout"]').click()
-        cy.title().should('eq',logInPage.loginMessage)
+        cy.title().should('eq', logInPage.loginMessage)
+    })
+    const PORT = Cypress.env("local.port");
+    it('AT_01.08_031|Log out button redirects to login page', function () {
+        cy.get('[href="/logout"]')
+            .should('be.visible')
+            .click();
+        cy.get('#loginIntroDefault')
+            .should('contain.text', logInPage.loginPageHeader);
+        cy.url()
+            .should('include', `http://localhost:${PORT}/login`);
     })
 
     it('AT_01.08_030|Header, Log out button is clickable and redirects to the login page', () => {
         cy.get('a[href="/logout"]').click();
         cy.get('[id=loginIntroDefault] h1').should('have.text', login.loginPageGreeting);
     });
-});
+})
