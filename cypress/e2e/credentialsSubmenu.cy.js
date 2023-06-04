34ca7d27215dcd1f3ef3501e7bc2097d7cc7d586
Naz <106405355+Nzs1@users.noreply.github.com>
2023-06-04 07:54:16 +0100

    AT_01.07_009 | Verify Redirection (#766)

--- cypress/e2e/credentialsSubmenu.cy.js
+++ cypress/e2e/credentialsSubmenu.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress"/>
+import headerCredentials from '../fixtures/headerCredentials.json'
 
 describe('credentialsSubmenu', () => {
+
+    beforeEach(() => {
+        cy.get('header .jenkins-menu-dropdown-chevron').realHover().click();
+        cy.get('[href="/user/admin/credentials"] span').invoke('text').as('h1')
+      })
 
     it('AT_01.07_01 <Header> Credentials menu redirects to the corresponding page', () => {
         cy.get('#page-header .model-link').realHover();
@@ -9,4 +15,12 @@ describe('credentialsSubmenu', () => {
         cy.url().should('contain', 'credentials');
         cy.get('h1').contains('Credentials');
     })
+
+
+    it('AT_01.07_009 | Verify Redirection to Credential Page from User Dropdown Menu', function () {
+        cy.get('header .jenkins-menu-dropdown-chevron').realHover().click();
+        cy.get('a[href="/user/admin/credentials"]').click()
+        cy.url().should('include', headerCredentials.credentialsPageUrl)
+        cy.get('.jenkins-app-bar__content h1').should('have.text', this.h1)
+      });
 })
