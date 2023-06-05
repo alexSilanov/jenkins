3fb14160b3df79c8ec1bd5a21c1de1fa79811c88
AleksaKhGitHub <aleksa.khouryna@gmail.com>
2023-06-05 23:10:20 +0300

    Commit2

--- cypress/pageObjects/MyViewPage.js
+++ cypress/pageObjects/MyViewPage.js
@@ -8,6 +8,7 @@ class MyViewPage {
   getBreadcrumbMyViewsItem = () => cy.get('li:nth-child(5) a:nth-child(1)');
   getPipelineNameLink = () =>  cy.get('a[href^="job/"].jenkins-table__link');
   getFreestyleProjectNameLink = () =>  cy.get('a[href^="job/"].jenkins-table__link');
+  getDashboardMyViewsLink = () => cy.get('#breadcrumbBar a[href="/user/admin/my-views/"]');
 
   clickNewItemSideMenuLink() {
     this.getNewItemSideMenuLink().click();
