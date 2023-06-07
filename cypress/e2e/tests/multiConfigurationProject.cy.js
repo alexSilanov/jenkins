fd5033017207fa3b0c06dbd590d03bea1a4c2d0f
shablouskaya <130236407+shablouskaya@users.noreply.github.com>
2023-06-06 20:00:50 -0400

    [RF} AT_14.06.003 Rename Multi-configuration project with the current name (#843)

--- cypress/e2e/tests/multiConfigurationProject.cy.js
+++ cypress/e2e/tests/multiConfigurationProject.cy.js
@@ -28,6 +28,7 @@ describe("multiConfigurationProject", () => {
     });
 
     it('AT_14.06.003 | Rename Multi-configuration project with the current name', () =>{
+        cy.createMultiConfigurationProject(name);
         headerAndFooter
         .clickJenkinsHomeLink()
         .clickProjectDrpDwnBtn()
