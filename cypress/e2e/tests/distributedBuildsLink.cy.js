cfaf91d2d5acc765cdf19df787696273bdc133c0
nadya0101 <kulikova0101@gmail.com>
2023-06-16 11:48:47 +0200

    [RF] | distributedBuildsLink (#993)

--- cypress/e2e/tests/distributedBuildsLink.cy.js
+++ cypress/e2e/tests/distributedBuildsLink.cy.js
@@ -1,18 +1,14 @@
 /// <reference types="cypress"/>
 import HomePage from "../../pageObjects/HomePage";
-import DistributedBuildsLinkPage from "../../pageObjects/DistributedBuildsLinkPage"
 import {distributedBuildsLinkPageUrl} from "../../fixtures/pom_fixtures/distributedBuildsLinkPageData.json"
 
 describe ('distributedBuildsLink', () => {
     const homePage = new HomePage();
-    const distributedBuildsLinkPage = new DistributedBuildsLinkPage;
   
   it ('AT_02.05.09 | Verify Link Learn more about distributed builds is redirected to the new window', () => {
     
     homePage
     .clickLearnMoreAboutDistributedBuildsLink()
-    distributedBuildsLinkPage
     .getDistributedBuildsLinkPageUrl().should('contain', distributedBuildsLinkPageUrl)
-   
   })
 })  
